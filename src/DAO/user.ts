d3dd1d300d6848b1ec89f156c2c4b803933e59a6
Akosua Asante <akosuaasante@gmail.com>
2019-01-26 01:00:32 -0500

    set up database stuff

--- src/DAO/user.ts
+++ src/DAO/user.ts
@@ -1,25 +1,29 @@
-// import {Repository} from "typeorm";
-import User, { Role } from "../models/user";
+import { getConnection, Repository } from "typeorm";
+import User from "../models/user";
 
 export default class UserDAO {
+    private userDb: Repository<User>;
     // constructor(private userDb: Repository<User>) {}
+    constructor() {
+        this.userDb = getConnection(process.env.NODE_ENV).getRepository("User");
+    }
+
+    public async getAllUsers(): Promise<User[]> {
+        return await this.userDb.find();
+    }
 
     public async getUserById(id: number): Promise<User> {
-        if (id === 100) {
-            return new User({id: 100, roles: [Role.ADMIN]});
-        }
-        return new User({id: 1});
-        // return await this.userDb.findOne(id);
+        return await this.userDb.findOneOrFail(id);
     }
 
     public async findUser(query: Partial<User>): Promise<User> {
-        if (query && query.email && query.email.includes("admin")) {
-            return new User({...query, id: 100});
-        }
-        return new User({id: 2, ...query});
+        return await this.userDb.findOneOrFail({ where: query });
     }
 
     public async createUser(userObj: Partial<User>): Promise<User> {
-        return new User(userObj);
+        // TODO: Perhaps some custom validation that the db can't be responsible for?
+        // Currently doesn't seem to be necessary
+        const user = new User(userObj);
+        return await this.userDb.save(user);
     }
 }
