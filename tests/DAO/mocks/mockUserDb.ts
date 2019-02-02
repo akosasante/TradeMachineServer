8828d8fefcbdc73fdeaf7654fc894fc7c60abf8f
Akosua Asante <akosuaasante@gmail.com>
2019-02-01 19:57:07 -0500

    dao controller unit test

---
+++ tests/DAO/mocks/mockUserDb.ts
@@ -0,0 +1,43 @@
+import logger from "../../../src/bootstrap/logger";
+import User from "../../../src/models/user";
+
+export const testUser = new User({id: 1, name: "Jatheesh", password: "pswd"});
+
+export default {
+    find: jest.fn()
+        .mockImplementationOnce(async () => {
+            logger.debug("mocking for getAllUsers");
+            return await [testUser];
+        }),
+    findOneOrFail: jest.fn()
+        .mockImplementationOnce(async id => {
+            logger.debug("mocking for getByUserById");
+            return testUser;
+        })
+        .mockImplementationOnce(async queryObj => {
+            logger.debug("mocking for findUser");
+            return testUser;
+        })
+        .mockImplementationOnce(async id => {
+            logger.debug("mocking for updateUser");
+            return testUser;
+        }),
+    findOne: jest.fn()
+        .mockImplementationOnce(async queryObj => {
+            logger.debug("mocking for findUser with false param");
+            return testUser;
+        }),
+    save: jest.fn()
+        .mockImplementationOnce(async user => {
+            logger.debug("mocking for createUser");
+            return user;
+        }),
+    update: jest.fn()
+        .mockImplementationOnce(async (query, userObj) => {
+            return { updated: true };
+        }),
+    delete: jest.fn()
+        .mockImplementationOnce(async id => {
+            return { deleted: true };
+        }),
+};
---
+++ tests/DAO/userDAO.test.ts
@@ -0,0 +1,108 @@
+import * as typeorm from "typeorm";
+import UserDAO from "../../src/DAO/user";
+import mockUserDb, { testUser } from "./mocks/mockUserDb";
+
+jest.spyOn(typeorm, "getConnection")
+    .mockReturnValue({getRepository: jest.fn().mockReturnValue(mockUserDb)});
+
+describe("UserDAO", () => {
+    let userDAO: UserDAO;
+
+    describe("getAllUsers", () => {
+        beforeAll(() => {
+            userDAO = new UserDAO();
+        });
+
+        it("should return an array of users as result of db call", async () => {
+            const res = await userDAO.getAllUsers();
+            // Testing that the correct db function is called with the correct params
+            expect(mockUserDb.find).toHaveBeenCalledTimes(1);
+            expect(mockUserDb.find).toHaveBeenCalledWith();
+            // Testing that we return as expected
+            expect(res).toEqual([testUser]);
+        });
+    });
+
+    describe("getUserById", () => {
+        beforeAll(() => {
+            userDAO = new UserDAO();
+        });
+
+        it("should return a single user as result of db call", async () => {
+            const res = await userDAO.getUserById(testUser.id!);
+            // Testing that the correct db function is called with the correct params
+            expect(mockUserDb.findOneOrFail).toHaveBeenCalledTimes(1);
+            expect(mockUserDb.findOneOrFail).toHaveBeenCalledWith(testUser.id);
+            // Testing that we return as expected
+            expect(res).toEqual(testUser);
+        });
+    });
+
+    describe("findUser", () => {
+        beforeAll(() => {
+            userDAO = new UserDAO();
+        });
+
+        it("should pass a query object to db and return a single user", async () => {
+            const res = await userDAO.findUser({email: testUser.email});
+            // Testing that the correct db function is called with the correct params
+            expect(mockUserDb.findOneOrFail).toHaveBeenCalledTimes(2);
+            expect(mockUserDb.findOneOrFail).toHaveBeenCalledWith({where: {email: testUser.email}});
+            // Testing that we return as expected
+            expect(res).toEqual(testUser);
+        });
+        it("should use the findOne method if the param passed is false", async () => {
+            const res = await userDAO.findUser({email: testUser.email}, false);
+            expect(mockUserDb.findOne).toHaveBeenCalledTimes(1);
+            expect(mockUserDb.findOne).toHaveBeenCalledWith({where: {email: testUser.email}});
+            expect(res).toEqual(testUser);
+        });
+    });
+
+    describe("createUser", () => {
+        beforeAll(() => {
+            userDAO = new UserDAO();
+        });
+
+        it("should a user instance from the object passed in", async () => {
+            const res = await userDAO.createUser(testUser.parse());
+            // Testing that the correct db function is called with the correct params
+            expect(mockUserDb.save).toHaveBeenCalledTimes(1);
+            expect(mockUserDb.save).toHaveBeenCalledWith(testUser);
+            // Testing that we return as expected
+            expect(res).toEqual(testUser);
+        });
+    });
+
+    describe("updateUser", () => {
+        beforeAll(() => {
+            userDAO = new UserDAO();
+        });
+
+        it("should call getUsrsById and associated db call then return single user", async () => {
+            const res = await userDAO.updateUser(testUser.id!, testUser.parse());
+            // Testing that the correct db function is called with the correct params
+            expect(mockUserDb.update).toHaveBeenCalledTimes(1);
+            expect(mockUserDb.update).toHaveBeenCalledWith({id: testUser.id}, testUser.parse());
+            expect(mockUserDb.findOneOrFail).toHaveBeenCalledTimes(3);
+            expect(mockUserDb.findOneOrFail).toHaveBeenCalledWith(testUser.id);
+            // Testing that we return as expected
+            expect(res).toEqual(testUser);
+        });
+    });
+
+    describe("deleteUser", () => {
+        beforeAll(() => {
+            userDAO = new UserDAO();
+        });
+
+        it("should return a delete result", async () => {
+            const res = await userDAO.deleteUser(testUser.id!);
+            // Testing that the correct db function is called with the correct params
+            expect(mockUserDb.delete).toHaveBeenCalledTimes(1);
+            expect(mockUserDb.delete).toHaveBeenCalledWith(testUser.id);
+            // Testing that we return as expected
+            expect(res).toEqual({ deleted: true });
+        });
+    });
+});
