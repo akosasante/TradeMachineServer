f2ed7cc84e6add682f521d2a303b926a45ec90d4
Akosua Asante <akosuaasante@gmail.com>
2019-02-09 15:41:00 -0500

    some updates for production

--- src/bootstrap/logger.ts
+++ src/bootstrap/logger.ts
@@ -55,7 +55,7 @@ const logger = winston.createLogger(({
     exitOnError: false,
 }));
 
-if (process.env.NODE_ENV !== "production") {
+if (process.env.ENABLE_LOGS === "true") {
     logger.add(consoleLogger);
 }
 
