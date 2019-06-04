36ea8782b29d756af6d5535a854a3194b91f923b
Akosua Asante <akosuaasante@gmail.com>
2019-06-04 16:49:16 -0400

    update settings controller with general settings methods

--- tests/unit/api/routes/SettingsController.test.ts
+++ tests/unit/api/routes/SettingsController.test.ts
@@ -3,6 +3,7 @@ import "jest-extended";
 import { EntityNotFoundError } from "typeorm/error/EntityNotFoundError";
 import SettingsController from "../../../../src/api/routes/SettingsController";
 import SettingsDAO, { ScheduleGetAllOptions } from "../../../../src/DAO/SettingsDAO";
+import GeneralSettings, { TradeDeadlineStatus } from "../../../../src/models/generalSettings";
 import ScheduledDowntime from "../../../../src/models/scheduledDowntime";
 
 describe("SettingsController", () => {
@@ -13,9 +14,17 @@ describe("SettingsController", () => {
         createScheduledDowntime: jest.fn(),
         updateScheduledDowntime: jest.fn(),
         deleteScheduledDowntime: jest.fn(),
+        getAllGeneralSettings: jest.fn(),
+        getSettingsById: jest.fn(),
+        getMostRecentSettings: jest.fn(),
+        insertNewSettingsLine: jest.fn(),
     };
 
     const testSchedule = new ScheduledDowntime({startTime: new Date(), endTime: new Date()});
+    const startDate = new Date("January 1 2019 1:00PM");
+    const endDate = new Date("January 1 2019 5:00PM");
+    const deadline = {status: TradeDeadlineStatus.ON, startTime: startDate, endTime: endDate};
+    const testSettings = new GeneralSettings({deadline});
     const settingsController = new SettingsController(mockSettingsDAO as unknown as SettingsDAO);
 
     afterEach(() => {
@@ -24,131 +33,203 @@ describe("SettingsController", () => {
         });
     });
 
-    describe("getAllScheduledDowntime", () => {
-        mockSettingsDAO.getAllScheduledDowntimes.mockReturnValue([testSchedule]);
-        it("should return an array schedules if no param is passed", async () => {
-            const res = await settingsController.getAllScheduledDowntime();
+    describe("Scheduled Downtime Methods", () => {
+        describe("getAllScheduledDowntime", () => {
+            mockSettingsDAO.getAllScheduledDowntimes.mockReturnValue([testSchedule]);
+            it("should return an array of schedules if no param is passed", async () => {
+                const res = await settingsController.getAllScheduledDowntime();
 
-            expect(mockSettingsDAO.getAllScheduledDowntimes).toHaveBeenCalledTimes(1);
-            expect(mockSettingsDAO.getAllScheduledDowntimes).toHaveBeenCalledWith(undefined);
-            expect(res).toEqual([testSchedule]);
-        });
-
-        it("should call getAllScheduledDOwntimes with FUTURE if param passed", async () => {
-            const res = await settingsController.getAllScheduledDowntime("future");
-
-            expect(mockSettingsDAO.getAllScheduledDowntimes).toHaveBeenCalledTimes(1);
-            expect(mockSettingsDAO.getAllScheduledDowntimes).toHaveBeenCalledWith(ScheduleGetAllOptions.FUTURE);
-            expect(res).toEqual([testSchedule]);
-        });
-
-        it("should call getAllScheduledDowntimes with PREVIOUS if param passed", async () => {
-            const res = await settingsController.getAllScheduledDowntime("previous");
-
-            expect(mockSettingsDAO.getAllScheduledDowntimes).toHaveBeenCalledTimes(1);
-            expect(mockSettingsDAO.getAllScheduledDowntimes).toHaveBeenCalledWith(ScheduleGetAllOptions.PREVIOUS);
-            expect(res).toEqual([testSchedule]);
-        });
-
-        it("should bubble up any errors from the DAO", async () => {
-            mockSettingsDAO.getAllScheduledDowntimes.mockImplementation(() => {
-                throw new Error("Generic Error");
+                expect(mockSettingsDAO.getAllScheduledDowntimes).toHaveBeenCalledTimes(1);
+                expect(mockSettingsDAO.getAllScheduledDowntimes).toHaveBeenCalledWith(undefined);
+                expect(res).toEqual([testSchedule]);
             });
-            await expect(settingsController.getAllScheduledDowntime()).rejects.toThrow(Error);
+
+            it("should call getAllScheduledDOwntimes with FUTURE if param passed", async () => {
+                const res = await settingsController.getAllScheduledDowntime("future");
+
+                expect(mockSettingsDAO.getAllScheduledDowntimes).toHaveBeenCalledTimes(1);
+                expect(mockSettingsDAO.getAllScheduledDowntimes).toHaveBeenCalledWith(ScheduleGetAllOptions.FUTURE);
+                expect(res).toEqual([testSchedule]);
+            });
+
+            it("should call getAllScheduledDowntimes with PREVIOUS if param passed", async () => {
+                const res = await settingsController.getAllScheduledDowntime("previous");
+
+                expect(mockSettingsDAO.getAllScheduledDowntimes).toHaveBeenCalledTimes(1);
+                expect(mockSettingsDAO.getAllScheduledDowntimes).toHaveBeenCalledWith(ScheduleGetAllOptions.PREVIOUS);
+                expect(res).toEqual([testSchedule]);
+            });
+
+            it("should bubble up any errors from the DAO", async () => {
+                mockSettingsDAO.getAllScheduledDowntimes.mockImplementation(() => {
+                    throw new Error("Generic Error");
+                });
+                await expect(settingsController.getAllScheduledDowntime()).rejects.toThrow(Error);
+            });
+        });
+
+        describe("getCurrentlyScheduledDowntime", () => {
+            it("should return a current schedule", async () => {
+                mockSettingsDAO.getCurrentlyScheduledDowntime.mockReturnValueOnce(testSchedule);
+                const res = await settingsController.getCurrentlyScheduledDowntime();
+
+                expect(mockSettingsDAO.getCurrentlyScheduledDowntime).toHaveBeenCalledTimes(1);
+                expect(mockSettingsDAO.getCurrentlyScheduledDowntime).toHaveBeenCalledWith();
+                expect(res).toEqual(testSchedule);
+            });
+
+            it("should throw an error if there is no current schedule", async () => {
+                mockSettingsDAO.getCurrentlyScheduledDowntime.mockImplementation(() => {
+                    throw new EntityNotFoundError(ScheduledDowntime, "ID not found.");
+                });
+                await expect(settingsController.getCurrentlyScheduledDowntime()).rejects.toThrow(EntityNotFoundError);
+            });
+        });
+
+        describe("getOneScheduledDowntime", () => {
+            it("should return a schedule by id", async () => {
+                const ID = 1;
+                mockSettingsDAO.getScheduledDowntimeById.mockReturnValue(testSchedule);
+                const res = await settingsController.getOneScheduledDowntime(ID);
+
+                expect(mockSettingsDAO.getScheduledDowntimeById).toHaveBeenCalledTimes(1);
+                expect(mockSettingsDAO.getScheduledDowntimeById).toHaveBeenCalledWith(ID);
+                expect(res).toEqual(testSchedule);
+            });
+
+            it("should throw an error if schedule is not found in db", async () => {
+                mockSettingsDAO.getScheduledDowntimeById.mockImplementation(() => {
+                    throw new EntityNotFoundError(ScheduledDowntime, "ID not found.");
+                });
+                await expect(settingsController.getOneScheduledDowntime(999)).rejects.toThrow(EntityNotFoundError);
+            });
+        });
+
+        describe("createScheduledDowntime", () => {
+            it("should create a schedule", async () => {
+                mockSettingsDAO.createScheduledDowntime.mockReturnValueOnce(testSchedule);
+                const res = await settingsController.createScheduledDowntime(testSchedule.parse());
+
+                expect(mockSettingsDAO.createScheduledDowntime).toHaveBeenCalledTimes(1);
+                expect(mockSettingsDAO.createScheduledDowntime).toHaveBeenCalledWith(testSchedule.parse());
+                expect(res).toEqual(testSchedule);
+            });
+
+            it("should bubble up any errors from the DAO", async () => {
+                mockSettingsDAO.createScheduledDowntime.mockImplementation(() => {
+                    throw new Error("Generic Error");
+                });
+                await expect(settingsController.createScheduledDowntime(testSchedule.parse())).rejects.toThrow(Error);
+            });
+        });
+
+        describe("updateScheduledDowntime", () => {
+            it("should return updated schedule for given id", async () => {
+                const ID = 1;
+                mockSettingsDAO.updateScheduledDowntime.mockReturnValueOnce(testSchedule);
+                const res = await settingsController.updateScheduledDowntime(ID, testSchedule.parse());
+
+                expect(mockSettingsDAO.updateScheduledDowntime).toHaveBeenCalledTimes(1);
+                expect(mockSettingsDAO.updateScheduledDowntime).toHaveBeenCalledWith(ID, testSchedule.parse());
+                expect(res).toEqual(testSchedule);
+            });
+
+            it("should throw an error if entity is not found in db", async () => {
+                mockSettingsDAO.updateScheduledDowntime.mockImplementation(() => {
+                    throw new EntityNotFoundError(ScheduledDowntime, "ID not found.");
+                });
+                await expect(settingsController.updateScheduledDowntime(999, testSchedule.parse()))
+                    .rejects.toThrow(EntityNotFoundError);
+            });
+        });
+
+        describe("deleteScheduledDowntime", () => {
+            it("should return out custom delete result", async () => {
+                const ID = 1;
+                mockSettingsDAO.deleteScheduledDowntime.mockReturnValueOnce({ raw: [ [], ID ] });
+                const res = await settingsController.deleteScheduledDowntime(ID);
+
+                expect(mockSettingsDAO.deleteScheduledDowntime).toHaveBeenCalledTimes(1);
+                expect(mockSettingsDAO.deleteScheduledDowntime).toHaveBeenCalledWith(ID);
+                expect(res).toEqual({ deleteResult: true, id: ID });
+            });
+
+            it("should throw an error if entity is not found in db", async () => {
+                mockSettingsDAO.deleteScheduledDowntime.mockImplementation(() => {
+                    throw new EntityNotFoundError(ScheduledDowntime, "ID not found.");
+                });
+                await expect(settingsController.deleteScheduledDowntime(999)).rejects.toThrow(EntityNotFoundError);
+            });
         });
     });
 
-    describe("getCurrentlyScheduledDowntime", () => {
-        it("should return a current schedule", async () => {
-            mockSettingsDAO.getCurrentlyScheduledDowntime.mockReturnValueOnce(testSchedule);
-            const res = await settingsController.getCurrentlyScheduledDowntime();
-
-            expect(mockSettingsDAO.getCurrentlyScheduledDowntime).toHaveBeenCalledTimes(1);
-            expect(mockSettingsDAO.getCurrentlyScheduledDowntime).toHaveBeenCalledWith();
-            expect(res).toEqual(testSchedule);
-        });
-
-        it("should throw an error if there is no current schedule", async () => {
-            mockSettingsDAO.getCurrentlyScheduledDowntime.mockImplementation(() => {
-                throw new EntityNotFoundError(ScheduledDowntime, "ID not found.");
+    describe("General Settings Methods", () => {
+        describe("getAllGeneralSettings", () => {
+            it("should return an array of settings", async () => {
+                mockSettingsDAO.getAllGeneralSettings.mockReturnValue([testSettings]);
+                const res = await settingsController.getAllGeneralSettings();
+                expect(mockSettingsDAO.getAllGeneralSettings).toHaveBeenCalledTimes(1);
+                expect(mockSettingsDAO.getAllGeneralSettings).toHaveBeenCalledWith();
+                expect(res).toEqual([testSettings]);
             });
-            await expect(settingsController.getCurrentlyScheduledDowntime()).rejects.toThrow(EntityNotFoundError);
-        });
-    });
 
-    describe("getOneScheduledDowntime", () => {
-        it("should return a schedule by id", async () => {
-            const ID = 1;
-            mockSettingsDAO.getScheduledDowntimeById.mockReturnValue(testSchedule);
-            const res = await settingsController.getOneScheduledDowntime(ID);
-
-            expect(mockSettingsDAO.getScheduledDowntimeById).toHaveBeenCalledTimes(1);
-            expect(mockSettingsDAO.getScheduledDowntimeById).toHaveBeenCalledWith(ID);
-            expect(res).toEqual(testSchedule);
-        });
-
-        it("should throw an error if schedule is not found in db", async () => {
-            mockSettingsDAO.getScheduledDowntimeById.mockImplementation(() => {
-                throw new EntityNotFoundError(ScheduledDowntime, "ID not found.");
+            it("should bubble up any errors from the DAO", async () => {
+                mockSettingsDAO.getAllGeneralSettings.mockImplementation(() => {
+                    throw new Error("Generic Error");
+                });
+                await expect(settingsController.getAllGeneralSettings()).rejects.toThrow(Error);
             });
-            await expect(settingsController.getOneScheduledDowntime(999)).rejects.toThrow(EntityNotFoundError);
         });
-    });
+        describe("getOneGeneralSettings", () => {
+            it("should return a settings row by id", async () => {
+                const ID = 1;
+                mockSettingsDAO.getSettingsById.mockReturnValue(testSettings);
+                const res = await settingsController.getOneGeneralSettings(ID);
 
-    describe("createScheduledDowntime", () => {
-        it("should create a schedule", async () => {
-            mockSettingsDAO.createScheduledDowntime.mockReturnValueOnce(testSchedule);
-            const res = await settingsController.createScheduledDowntime(testSchedule.parse());
-
-            expect(mockSettingsDAO.createScheduledDowntime).toHaveBeenCalledTimes(1);
-            expect(mockSettingsDAO.createScheduledDowntime).toHaveBeenCalledWith(testSchedule.parse());
-            expect(res).toEqual(testSchedule);
-        });
-
-        it("should bubble up any errors from the DAO", async () => {
-            mockSettingsDAO.createScheduledDowntime.mockImplementation(() => {
-                throw new Error("Generic Error");
+                expect(mockSettingsDAO.getSettingsById).toHaveBeenCalledTimes(1);
+                expect(mockSettingsDAO.getSettingsById).toHaveBeenCalledWith(ID);
+                expect(res).toEqual(testSettings);
             });
-            await expect(settingsController.createScheduledDowntime(testSchedule.parse())).rejects.toThrow(Error);
-        });
-    });
-
-    describe("updateScheduledDowntime", () => {
-        it("should return updated schedule for given id", async () => {
-            const ID = 1;
-            mockSettingsDAO.updateScheduledDowntime.mockReturnValueOnce(testSchedule);
-            const res = await settingsController.updateScheduledDowntime(ID, testSchedule.parse());
-
-            expect(mockSettingsDAO.updateScheduledDowntime).toHaveBeenCalledTimes(1);
-            expect(mockSettingsDAO.updateScheduledDowntime).toHaveBeenCalledWith(ID, testSchedule.parse());
-            expect(res).toEqual(testSchedule);
-        });
-
-        it("should throw an error if entity is not found in db", async () => {
-            mockSettingsDAO.updateScheduledDowntime.mockImplementation(() => {
-                throw new EntityNotFoundError(ScheduledDowntime, "ID not found.");
+            it("should throw an error if id not found in db", async () => {
+                mockSettingsDAO.getSettingsById.mockImplementation(() => {
+                    throw new EntityNotFoundError(GeneralSettings, "ID not found.");
+                });
+                await expect(settingsController.getOneGeneralSettings(999)).rejects.toThrow(EntityNotFoundError);
             });
-            await expect(settingsController.updateScheduledDowntime(999, testSchedule.parse()))
-                .rejects.toThrow(EntityNotFoundError);
-        });
-    });
-
-    describe("deleteScheduledDowntime", () => {
-        it("should return out custom delete result", async () => {
-            const ID = 1;
-            mockSettingsDAO.deleteScheduledDowntime.mockReturnValueOnce({ raw: [ [], ID ] });
-            const res = await settingsController.deleteScheduledDowntime(ID);
-
-            expect(mockSettingsDAO.deleteScheduledDowntime).toHaveBeenCalledTimes(1);
-            expect(mockSettingsDAO.deleteScheduledDowntime).toHaveBeenCalledWith(ID);
-            expect(res).toEqual({ deleteResult: true, id: ID });
         });
 
-        it("should throw an error if entity is not found in db", async () => {
-            mockSettingsDAO.deleteScheduledDowntime.mockImplementation(() => {
-                throw new EntityNotFoundError(ScheduledDowntime, "ID not found.");
+        describe("getMostRecentSettings", () => {
+            it("should return the most recent settings", async () => {
+                mockSettingsDAO.getMostRecentSettings.mockReturnValue(testSettings);
+                const res = await settingsController.getMostRecentSettings();
+
+                expect(mockSettingsDAO.getMostRecentSettings).toHaveBeenCalledTimes(1);
+                expect(mockSettingsDAO.getMostRecentSettings).toHaveBeenCalledWith();
+                expect(res).toEqual(testSettings);
             });
-            await expect(settingsController.deleteScheduledDowntime(999)).rejects.toThrow(EntityNotFoundError);
+            it("should throw an error if there are no settings", async () => {
+                mockSettingsDAO.getMostRecentSettings.mockImplementation(() => {
+                    throw new EntityNotFoundError(GeneralSettings, "ID not found.");
+                });
+                await expect(settingsController.getMostRecentSettings()).rejects.toThrow(EntityNotFoundError);
+            });
+        });
+
+        describe("createNewSettings", () => {
+            it("should create a schedule", async () => {
+                mockSettingsDAO.insertNewSettingsLine.mockReturnValue(testSettings);
+                const res = await settingsController.createNewSettings(testSettings.parse());
+
+                expect(mockSettingsDAO.insertNewSettingsLine).toHaveBeenCalledTimes(1);
+                expect(mockSettingsDAO.insertNewSettingsLine).toHaveBeenCalledWith(testSettings.parse());
+                expect(res).toEqual(testSettings);
+            });
+            it("should bubble up any errors from the DAO", async () => {
+                mockSettingsDAO.insertNewSettingsLine.mockImplementation(() => {
+                    throw new Error("Generic Error");
+                });
+                await expect(settingsController.createNewSettings(testSettings.parse())).rejects.toThrow(Error);
+            });
         });
     });
 });
