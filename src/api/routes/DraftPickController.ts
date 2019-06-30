4cc6c966a5b3d73cf8e6c1bf1513df89fbb040e6
Akosua Asante <akosuaasante@gmail.com>
2019-06-30 19:24:40 -0400

    draft pick controller and test

---
+++ src/api/routes/DraftPickController.ts
@@ -0,0 +1,87 @@
+import {
+    Authorized,
+    Body,
+    Delete,
+    Get,
+    JsonController,
+    Param,
+    Post,
+    Put,
+    QueryParam,
+    QueryParams
+} from "routing-controllers";
+import { inspect } from "util";
+import logger from "../../bootstrap/logger";
+import DraftPickDAO from "../../DAO/DraftPickDAO";
+import DraftPick from "../../models/draftPick";
+import { LeagueLevel } from "../../models/player";
+import { Role } from "../../models/user";
+import { cleanupQuery } from "../ApiHelpers";
+
+@JsonController("/picks")
+export default class DraftPickController {
+    private dao: DraftPickDAO;
+
+    constructor(DAO?: DraftPickDAO) {
+        this.dao = DAO || new DraftPickDAO();
+    }
+
+    @Get("/")
+    public async getAllDraftPicks(@QueryParam("include") include?: string[]): Promise<DraftPick[]> {
+        logger.debug("get all draftPicks endpoint" + `${include ? "with params: " + include : ""}`);
+        let draftPicks: DraftPick[] = [];
+        if (include) {
+            const params = getAllDraftPicksQuery(include);
+            draftPicks = (await this.dao.findPicks(params)) || draftPicks;
+        } else {
+            draftPicks = (await this.dao.getAllPicks()) || draftPicks;
+        }
+        logger.debug(`got ${draftPicks.length} draftPicks`);
+        return draftPicks;
+    }
+
+    @Get("/:id([0-9]+)")
+    public async getOneDraftPick(@Param("id") id: number): Promise<DraftPick> {
+        logger.debug("get one draftPick endpoint");
+        return await this.dao.getPickById(id);
+    }
+
+    @Get("/search")
+    public async findDraftPicksByQuery(@QueryParams() query: Partial<DraftPick>): Promise<DraftPick[]> {
+        logger.debug(`searching for draftPick with props: ${inspect(query)}`);
+        return await this.dao.findPicks(cleanupQuery(query as {[key: string]: string}));
+    }
+
+    @Authorized(Role.ADMIN)
+    @Post("/")
+    public async createDraftPick(@Body() draftPickObj: Partial<DraftPick>): Promise<DraftPick> {
+        logger.debug("create team endpoint");
+        return await this.dao.createPick(draftPickObj);
+    }
+
+    @Authorized(Role.ADMIN)
+    @Put("/:id([0-9]+)")
+    public async updateDraftPick(@Param("id") id: number, @Body() draftPickObj: Partial<DraftPick>):
+        Promise<DraftPick> {
+        logger.debug("update draftPick endpoint");
+        return await this.dao.updatePick(id, draftPickObj);
+    }
+
+    @Authorized(Role.ADMIN)
+    @Delete("/:id([0-9]+)")
+    public async deleteDraftPick(@Param("id") id: number) {
+        logger.debug("delete draftPick endpoint");
+        const result = await this.dao.deletePick(id);
+        logger.debug(`delete successful: ${inspect(result)}`);
+        return await {deleteResult: !!result.raw[1], id};
+    }
+}
+
+function getAllDraftPicksQuery(includes: string[]) {
+    const keywordToLevelMap: {[key: string]: LeagueLevel} = {
+        high: LeagueLevel.HIGH,
+        low: LeagueLevel.LOW,
+        majors: LeagueLevel.MAJOR,
+    };
+    return includes.map(include => ({ type: keywordToLevelMap[include] }));
+}
---
+++ tests/unit/api/routes/DraftPickController.test.ts
@@ -0,0 +1,146 @@
+import "jest";
+import "jest-extended";
+import { EntityNotFoundError } from "typeorm/error/EntityNotFoundError";
+import DraftPickController from "../../../../src/api/routes/DraftPickController";
+import DraftPickDAO from "../../../../src/DAO/DraftPickDAO";
+import DraftPick from "../../../../src/models/draftPick";
+import { LeagueLevel } from "../../../../src/models/player";
+
+describe("DraftPickController", () => {
+    const mockDraftPickDAO = {
+        getAllPicks: jest.fn(),
+        getPickById: jest.fn(),
+        findPicks: jest.fn(),
+        createPick: jest.fn(),
+        updatePick: jest.fn(),
+        deletePick: jest.fn(),
+    };
+    const testDraftPick = new DraftPick({round: 1, pickNumber: 12, type: LeagueLevel.LOW});
+    const draftPickController = new DraftPickController(mockDraftPickDAO as unknown as DraftPickDAO);
+
+    afterEach(() => {
+        Object.entries(mockDraftPickDAO).forEach((kvp: [string, jest.Mock<any, any>]) => {
+            kvp[1].mockClear();
+        });
+    });
+
+    describe("getAllDraftPicks method", () => {
+        it("should return an array of draftPicks if no params is passed", async () => {
+            mockDraftPickDAO.getAllPicks.mockReturnValueOnce([testDraftPick]);
+            const res = await draftPickController.getAllDraftPicks();
+
+            expect(mockDraftPickDAO.findPicks).toHaveBeenCalledTimes(0);
+            expect(mockDraftPickDAO.getAllPicks).toHaveBeenCalledTimes(1);
+            expect(mockDraftPickDAO.getAllPicks).toHaveBeenCalledWith();
+            expect(res).toEqual([testDraftPick]);
+        });
+        it("should return an array of draftPicks if a param is passed and call the find method", async () => {
+            mockDraftPickDAO.findPicks.mockReturnValueOnce([testDraftPick]);
+            const res = await draftPickController.getAllDraftPicks(["high", "majors"]);
+
+            expect(mockDraftPickDAO.getAllPicks).toHaveBeenCalledTimes(0);
+            expect(mockDraftPickDAO.findPicks).toHaveBeenCalledTimes(1);
+            expect(mockDraftPickDAO.findPicks).toHaveBeenCalledWith([
+                {league: LeagueLevel.HIGH},
+                {league: LeagueLevel.MAJOR}]);
+            expect(res).toEqual([testDraftPick]);
+        });
+        it("should bubble up any errors from the DAO", async () => {
+            mockDraftPickDAO.getAllPicks.mockImplementation(() => {
+                throw new Error("Generic Error");
+            });
+            await expect(draftPickController.getAllDraftPicks())
+                .rejects.toThrow(Error);
+        });
+    });
+
+    describe("getOneDraftPick method", () => {
+        it("should return a draftPick by id", async () => {
+            mockDraftPickDAO.getPickById.mockReturnValue(testDraftPick);
+            const res = await draftPickController.getOneDraftPick(testDraftPick.id!);
+
+            expect(mockDraftPickDAO.getPickById).toHaveBeenCalledTimes(1);
+            expect(mockDraftPickDAO.getPickById).toHaveBeenCalledWith(testDraftPick.id);
+            expect(res).toEqual(testDraftPick);
+        });
+        it("should throw an error if entity is not found in db", async () => {
+            mockDraftPickDAO.getPickById.mockImplementation(() => {
+                throw new EntityNotFoundError(DraftPick, "ID not found.");
+            });
+            await expect(draftPickController.getOneDraftPick(9999))
+                .rejects.toThrow(EntityNotFoundError);
+        });
+    });
+
+    describe("findDraftPicksByQuery method", () => {
+        const query = {season: 2018};
+        it("should find draftPicks by the given query options", async () => {
+            mockDraftPickDAO.findPicks.mockReturnValue([testDraftPick]);
+            const res = await draftPickController.findDraftPicksByQuery(query);
+
+            expect(mockDraftPickDAO.findPicks).toHaveBeenCalledTimes(1);
+            expect(mockDraftPickDAO.findPicks).toHaveBeenCalledWith(query);
+            expect(res).toEqual([testDraftPick]);
+        });
+        it("should throw an error if entity is not found in db", async () => {
+            mockDraftPickDAO.findPicks.mockImplementation(() => {
+                throw new EntityNotFoundError(DraftPick, "ID not found.");
+            });
+            await expect(draftPickController.findDraftPicksByQuery(query)).rejects.toThrow(EntityNotFoundError);
+        });
+    });
+
+    describe("createDraftPick method", () => {
+        it("should create a draftPick", async () => {
+            mockDraftPickDAO.createPick.mockReturnValue(testDraftPick);
+            const res = await draftPickController.createDraftPick(testDraftPick.parse());
+
+            expect(mockDraftPickDAO.createPick).toHaveBeenCalledTimes(1);
+            expect(mockDraftPickDAO.createPick).toHaveBeenCalledWith(testDraftPick.parse());
+            expect(res).toEqual(testDraftPick);
+        });
+        it("should bubble up any errors from the DAO", async () => {
+            mockDraftPickDAO.createPick.mockImplementation(() => {
+                throw new Error("Generic Error");
+            });
+            await expect(draftPickController.createDraftPick(testDraftPick.parse()))
+                .rejects.toThrow(Error);
+        });
+    });
+
+    describe("updateDraftPick method", () => {
+        it("should return updated draftPick with the given id", async () => {
+            mockDraftPickDAO.updatePick.mockReturnValue(testDraftPick);
+            const res = await draftPickController.updateDraftPick(testDraftPick.id!, testDraftPick.parse());
+
+            expect(mockDraftPickDAO.updatePick).toHaveBeenCalledTimes(1);
+            expect(mockDraftPickDAO.updatePick).toHaveBeenCalledWith(testDraftPick.id, testDraftPick.parse());
+            expect(res).toEqual(testDraftPick);
+        });
+        it("should throw an error if entity is not found in db", async () => {
+            mockDraftPickDAO.updatePick.mockImplementation(() => {
+                throw new EntityNotFoundError(DraftPick, "ID not found.");
+            });
+            await expect(draftPickController.updateDraftPick(9999, testDraftPick.parse()))
+                .rejects.toThrow(EntityNotFoundError);
+        });
+    });
+
+    describe("deleteDraftPick method", () => {
+        it("should delete a draftPick by id from the db", async () => {
+            mockDraftPickDAO.deletePick.mockReturnValue({raw: [ [], testDraftPick.id ]});
+            const res = await draftPickController.deleteDraftPick(testDraftPick.id!);
+
+            expect(mockDraftPickDAO.deletePick).toHaveBeenCalledTimes(1);
+            expect(mockDraftPickDAO.deletePick).toHaveBeenCalledWith(testDraftPick.id);
+            expect(res).toEqual({deleteResult: true, id: testDraftPick.id});
+        });
+        it("should throw an error if entity is not found in db", async () => {
+            mockDraftPickDAO.deletePick.mockImplementation(() => {
+                throw new EntityNotFoundError(DraftPick, "ID not found.");
+            });
+            await expect(draftPickController.deleteDraftPick(9999))
+                .rejects.toThrow(EntityNotFoundError);
+        });
+    });
+});
