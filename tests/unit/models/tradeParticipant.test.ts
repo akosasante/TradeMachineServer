9f01d6764211c0797b248625f816bd92fc209d3a
Akosua Asante <akosuaasante@gmail.com>
2019-07-15 18:50:32 -0400

    trade participant + model test

---
+++ tests/unit/models/tradeParticipant.test.ts
@@ -0,0 +1,26 @@
+import "jest";
+import "jest-extended";
+import Team from "../../../src/models/team";
+import Trade from "../../../src/models/trade";
+import TradeParticipant, { TradeParticipantType } from "../../../src/models/tradeParticipant";
+
+describe("Trade Participant Class", () => {
+    const team = new Team({name: "Squirtle Squad", espnId: 1});
+    const trade = new Trade({id: 1, tradeItems: [], tradeParticipants: []});
+    const participant = new TradeParticipant({participantType: TradeParticipantType.RECIPIENT, trade, team});
+
+    describe("constructor", () => {
+        it("should construct the obj as expected", () => {
+            expect(participant.participantType).toEqual(TradeParticipantType.RECIPIENT);
+            expect(participant.trade).toEqual(trade);
+            expect(participant.team).toEqual(team);
+        });
+    });
+
+    describe("instance methods", () => {
+        it("toString/0", () => {
+            expect(participant.toString()).toMatch("for trade#");
+            expect(participant.toString()).toMatch("and team#");
+        });
+    });
+});
