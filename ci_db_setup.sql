861bb62fec4a2272469e77b5aebff6c2fdefadbb
Akosua Asante <akosuaasante@gmail.com>
2019-08-08 08:59:52 -0400

    uuid extension install

--- ci_db_setup.sql
+++ ci_db_setup.sql
@@ -1,4 +1,6 @@
 CREATE USER :user with password ':password';
 CREATE DATABASE :db_name owner :user;
+SELECT * FROM pg_available_extensions;
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 \connect :db_name;
 CREATE SCHEMA test AUTHORIZATION :user;
