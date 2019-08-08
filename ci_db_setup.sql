6cb28ce226f9f8774654c32b83e0eac00eca2898
Akosua Asante <akosuaasante@gmail.com>
2019-08-08 09:50:29 -0400

    clean up and get logs

--- ci_db_setup.sql
+++ ci_db_setup.sql
@@ -1,7 +1,7 @@
 CREATE USER :user with password ':password';
 CREATE DATABASE :db_name owner :user;
-SELECT * FROM pg_available_extensions;
+-- SELECT * FROM pg_available_extensions;
 \connect :db_name;
 CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-\df
+-- \df
 CREATE SCHEMA test AUTHORIZATION :user;
