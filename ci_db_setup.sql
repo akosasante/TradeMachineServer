136bf3e0bf53dfde2cab2a8f0b974d5c3924b8db
Akosua Asante <akosuaasante@gmail.com>
2019-08-08 08:34:16 -0400

    use quotes

--- ci_db_setup.sql
+++ ci_db_setup.sql
@@ -1,4 +1,4 @@
-CREATE USER :user with password :password;
+CREATE USER :user with password ':password';
 CREATE DATABASE :db_name owner :user;
 \connect :db_name;
 CREATE SCHEMA test AUTHORIZATION :user;
