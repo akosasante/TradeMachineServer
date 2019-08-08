428c0e98f68181812a12db19fa6104eeefe19146
Akosua Asante <akosuaasante@gmail.com>
2019-08-07 23:20:31 -0400

    fixing vulnerabilities

--- tests/integration/app.test.ts
+++ tests/integration/app.test.ts
@@ -15,7 +15,7 @@ async function shutdown() {
         });
     });
     // redis.quit() creates a thread to close the connection.
-    // We wait until all threads have been run once to ensure the connection closes.,,
+    // We wait until all threads have been run once to ensure the connection closes.
     await new Promise(resolve => setImmediate(resolve));
 }
 
