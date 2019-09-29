ac20324714149c351ae697832845cf760f64dbec
Akosua Asante <akosuaasante@gmail.com>
2019-09-29 11:05:05 -0400

    allow ngrok in local development

--- src/bootstrap/app.ts
+++ src/bootstrap/app.ts
@@ -14,7 +14,7 @@ export default async function(): Promise<Express> {
 
     // Register routes and some global auth middlewares
     logger.debug("setting up route-controllers");
-    const developmentOrigins = [/localhost:3000/, /localhost:8080/, /127\.0\.0\.1/];
+    const developmentOrigins = [/localhost:3000/, /localhost:8080/, /127\.0\.0\.1/, /ngrok/];
     const prodOrigins: Array<string|RegExp> = [];
     const allowedOrigins = prodOrigins.concat(process.env.NODE_ENV === "development" ? developmentOrigins : []);
 
