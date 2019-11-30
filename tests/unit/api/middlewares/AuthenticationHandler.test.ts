36ae6b31223386f9cf45742dfb67901f69c28c4b
Akosua Asante <akosuaasante@gmail.com>
2019-11-30 14:38:28 -0500

    authenticationhandler refactor - registration handler

--- tests/unit/api/middlewares/AuthenticationHandler.test.ts
+++ tests/unit/api/middlewares/AuthenticationHandler.test.ts
@@ -1,30 +1,24 @@
+import { hash } from "bcryptjs";
 import { NextFunction, Request, Response } from "express";
 import "jest";
 import "jest-extended";
 import { UnauthorizedError } from "routing-controllers";
-import { LoginHandler } from "../../../../src/api/middlewares/AuthenticationHandler";
+import { LoginHandler, RegisterHandler } from "../../../../src/api/middlewares/AuthenticationHandler";
 import { ConflictError } from "../../../../src/api/middlewares/ErrorHandler";
-import * as AuthFuncs from "../../../../src/authentication/auth";
-import User from "../../../../src/models/user";
 import UserDAO from "../../../../src/DAO/UserDAO";
-import {UserFactory} from "../../../factories/UserFactory";
-import {hash} from "bcryptjs";
+import UserDO from "../../../../src/models/user";
+import { UserFactory } from "../../../factories/UserFactory";
 
 const mockUserDAO = {
-    // getAllUsers: jest.fn(),
-    // getAllUsersWithTeams: jest.fn(),
-    // getUserById: jest.fn(),
     findUser: jest.fn(),
     getUserPassword: jest.fn(),
-    // createUsers: jest.fn(),
     updateUser: jest.fn(),
-    // deleteUser: jest.fn(),
 };
 
 describe("Authentication middleware", () => {
     const testUser = UserFactory.getUser("j@gm.com", "Jatheesh", undefined, undefined, {id: "d4e3fe52-1b18-4cb6-96b1-600ed86ec45b"});
     const testUserModel = testUser.toUserModel();
-    
+
     describe("LoginHandler", () => {
         it("should serialize the user and return the next function if sign in was successful", async () => {
             mockUserDAO.findUser.mockReturnValueOnce(testUserModel);
@@ -41,7 +35,7 @@ describe("Authentication middleware", () => {
             const response: Response  = {};
             const loginHandler = new LoginHandler(mockUserDAO as unknown as UserDAO);
             await loginHandler.use(request, response, next);
-            
+
             expect(request.session!.save).toBeCalledTimes(1);
             expect(request.session!.user).toBeDefined();
             expect(request.session!.user).toEqual(testUser.id);
@@ -60,48 +54,51 @@ describe("Authentication middleware", () => {
             const response: Response  = {};
             const loginHandler = new LoginHandler(mockUserDAO as unknown as UserDAO);
             await loginHandler.use(request, response, next);
-            
+
             expect(next).toBeCalledTimes(1);
             expect(next).toBeCalledWith(new UnauthorizedError("User could not be authenticated. Incorrect password"));
             expect(request.session!.destroy).toBeCalledTimes(1);
             expect(request.session!.user).toBeUndefined();
         });
     });
-    // describe("Register Handler", () => {
-    //     it("should serialize the user and return the next function if sign up was successful", async () => {
-    //         // @ts-ignore
-    //         AuthFuncs.signUpAuthentication = jest.fn((mail, psswrd, done) => {
-    //             return done(undefined, testUser);
-    //         });
-    //         const next: NextFunction = jest.fn();
-    //         // @ts-ignore
-    //         const request: Request = {body: {email, password}, session: {}};
-    //         // @ts-ignore
-    //         const response: Response  = {};
-    //         const registerHandler = new RegisterHandler();
-    //         await registerHandler.use(request, response, next);
-    //
-    //         expect(next).toBeCalledTimes(1);
-    //         expect(next).toBeCalledWith();
-    //         expect(request.session!.user).toBeDefined();
-    //         expect(request.session!.user).toEqual(testUser.id);
-    //     });
-    //     it("should return the original error if the signup method returns an error", async () => {
-    //         // @ts-ignore
-    //         AuthFuncs.signUpAuthentication = jest.fn((mail, psswrd, done) => {
-    //             return done(new ConflictError("Email already in use and signed up."));
-    //         });
-    //         const next: NextFunction = jest.fn();
-    //         // @ts-ignore
-    //         const request: Request = {body: {email, password}, session: {}};
-    //         // @ts-ignore
-    //         const response: Response  = {};
-    //         const registerHandler = new RegisterHandler();
-    //         await registerHandler.use(request, response, next);
-    //
-    //         expect(next).toBeCalledTimes(1);
-    //         expect(next).toBeCalledWith(new ConflictError("Email already in use and signed up."));
-    //         expect(request.session!.user).toBeUndefined();
-    //     });
-    // });
+    describe("Register Handler", () => {
+        it("should serialize the user and return the next function if sign up was successful", async () => {
+            const passwordlessUser = new UserDO({...testUser, password: undefined});
+            const passwordlessUserModel = passwordlessUser.toUserModel();
+            mockUserDAO.findUser.mockReturnValueOnce(passwordlessUserModel);
+            mockUserDAO.updateUser.mockReturnValueOnce(testUserModel);
+            const next: NextFunction = jest.fn();
+            const request: Request = {
+                body: {email: testUser.email!, password: testUser.password!},
+                // @ts-ignore
+                session: {},
+            };
+            // @ts-ignore
+            const response: Response  = {};
+            const registerHandler = new RegisterHandler(mockUserDAO as unknown as UserDAO);
+            await registerHandler.use(request, response, next);
+
+            expect(next).toBeCalledTimes(1);
+            expect(next).toBeCalledWith();
+            expect(request.session!.user).toBeDefined();
+            expect(request.session!.user).toEqual(testUser.id);
+        });
+        it("should return the original error if the signup method returns an error", async () => {
+            mockUserDAO.findUser.mockReturnValueOnce(testUserModel);
+            const next: NextFunction = jest.fn();
+            const request: Request = {
+                body: {email: testUser.email!, password: testUser.password!},
+                // @ts-ignore
+                session: {},
+            };
+            // @ts-ignore
+            const response: Response  = {};
+            const registerHandler = new RegisterHandler(mockUserDAO as unknown as UserDAO);
+            await registerHandler.use(request, response, next);
+
+            expect(next).toBeCalledTimes(1);
+            expect(next).toBeCalledWith(new ConflictError("Email already in use and signed up."));
+            expect(request.session!.user).toBeUndefined();
+        });
+    });
 });
