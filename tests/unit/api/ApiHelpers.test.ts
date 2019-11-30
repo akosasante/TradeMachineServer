b7be271e34ff64d3d5222e35ff4ac2cd76985300
Akosua Asante <akosuaasante@gmail.com>
2019-11-29 21:18:15 -0500

    cleanup ApiHelper and test

--- tests/unit/api/ApiHelpers.test.ts
+++ tests/unit/api/ApiHelpers.test.ts
@@ -4,25 +4,22 @@ import { cleanupQuery } from "../../../src/api/ApiHelpers";
 describe("ApiHelpers utility functions", () => {
     describe("cleanupQuery/1", () => {
         const baseQuery: any = { username: "aasante" };
-        it("should remove the 'multiple' key from the query obj", () => {
-            const query = {...baseQuery, multiple: "true"};
-            const returned: any = cleanupQuery(query);
-            expect(returned.multiple).toBeUndefined();
-        });
         it("should turn all of the accepted values into ISNULL", () => {
             const keys = ["null", "undefined"];
             for (const key of keys) {
                 const query = {...baseQuery, field: key};
                 const returned: any = cleanupQuery(query);
-                expect(returned.field.value).toEqual(IsNull().value); // undefined
+                expect(returned.field.value).toEqual(IsNull().value);
+                expect(returned.field._type).toEqual("isNull");
             }
         });
         it("should turn all of the accepted values into ISNOTNULLs", () => {
-            const keys = ["null", "undefined"];
+            const keys = ["not null", "!undefined"];
             for (const key of keys) {
                 const query = {...baseQuery, field: key};
                 const returned: any = cleanupQuery(query);
-                expect(returned.field.value).toEqual(Not(IsNull()).value); //
+                expect(returned.field.value).toEqual((Not(IsNull())).value);
+                expect(returned.field._type).toEqual("not");
             }
         });
     });
