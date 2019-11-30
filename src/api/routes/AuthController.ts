1fac84ad957286e39b550ae29bcab837da744866
Akosua Asante <akosuaasante@gmail.com>
2019-11-30 14:50:15 -0500

    authcontroller refactor - login and signup

--- src/api/routes/AuthController.ts
+++ src/api/routes/AuthController.ts
@@ -1,10 +1,10 @@
-import { Request, Response } from "express";
-import { BodyParam, Controller, Post, Req, Res, Session, UseBefore } from "routing-controllers";
-import { deserializeUser } from "../../bootstrap/auth";
+import {Request, Response} from "express";
+import {BodyParam, Controller, Post, Req, Res, Session, UseBefore} from "routing-controllers";
+import {deserializeUser} from "../../authentication/auth";
 import logger from "../../bootstrap/logger";
 import UserDAO from "../../DAO/UserDAO";
-import User from "../../models/user";
-import { LoginHandler, RegisterHandler } from "../middlewares/AuthenticationHandler";
+import {LoginHandler, RegisterHandler} from "../middlewares/AuthenticationHandler";
+import {User} from "@akosasante/trade-machine-models";
 
 @Controller("/auth")
 export default class AuthController {
@@ -18,61 +18,59 @@ export default class AuthController {
     @UseBefore(LoginHandler)
     public async login(@Req() request: Request, @Session() session: any): Promise<User> {
         // TODO: Maybe extract deserialization to a usebefore api.middleware
-        const user = await deserializeUser(session.user);
-        return user!.publicUser;
+        return await deserializeUser(session.user, this.userDao);
     }
 
     @Post("/signup")
     @UseBefore(RegisterHandler)
     public async signup(@Req() request: Request, @Session() session: any): Promise<User> {
-        const user = await deserializeUser(session.user);
-        return user!.publicUser;
+        return await deserializeUser(session.user, this.userDao);
     }
 
-    @Post("/logout")
-    public async logout(@Req() request: Request, @Session() session: any) {
-        return new Promise((resolve, reject) => {
-            if (session && session.user && request.session) {
-                request.session.destroy(async err => {
-                    if (err) {
-                        logger.error("Error destroying session");
-                        reject(err);
-                    } else {
-                        logger.debug(`Destroying user session for userId#${session.user}`);
-                        await this.userDao.updateUser(session.user, { lastLoggedIn: new Date() });
-                        delete session.user;
-                        resolve(true);
-                    }
-                });
-            } else {
-                // Assumedly, there's nothing to log out of. We're all good.
-                // I don't think it's necessary to throw an error here
-                logger.debug("Resolving empty session logout");
-                resolve(true);
-            }
-        });
-    }
-
-    @Post("/reset_password")
-    public async resetPassword(@BodyParam("id") userId: number,
-                               @BodyParam("password") newPassword: string,
-                               @BodyParam("token") passwordResetToken: string,
-                               @Res() response: Response): Promise<Response> {
-        const hashedPassword = await User.generateHashedPassword(newPassword);
-        const existingUser = await this.userDao.findUser({id: userId, passwordResetToken}, false);
-
-        if (!existingUser) {
-            logger.debug("did not find user for id and token");
-            return response.status(404).json("user does not exist");
-        }
-        if (!existingUser.passwordResetIsValid()) {
-            logger.debug("user password reset expired");
-            return response.status(403).json("expired");
-        }
-
-        await this.userDao.updateUser(userId, {
-            password: hashedPassword,
-            passwordResetExpiresOn: undefined });
-        return response.status(200).json("success");
-    }
+    // @Post("/logout")
+    // public async logout(@Req() request: Request, @Session() session: any) {
+    //     return new Promise((resolve, reject) => {
+    //         if (session && session.user && request.session) {
+    //             request.session.destroy(async err => {
+    //                 if (err) {
+    //                     logger.error("Error destroying session");
+    //                     reject(err);
+    //                 } else {
+    //                     logger.debug(`Destroying user session for userId#${session.user}`);
+    //                     await this.userDao.updateUser(session.user, { lastLoggedIn: new Date() });
+    //                     delete session.user;
+    //                     resolve(true);
+    //                 }
+    //             });
+    //         } else {
+    //             // Assumedly, there's nothing to log out of. We're all good.
+    //             // I don't think it's necessary to throw an error here
+    //             logger.debug("Resolving empty session logout");
+    //             resolve(true);
+    //         }
+    //     });
+    // }
+    //
+    // @Post("/reset_password")
+    // public async resetPassword(@BodyParam("id") userId: number,
+    //                            @BodyParam("password") newPassword: string,
+    //                            @BodyParam("token") passwordResetToken: string,
+    //                            @Res() response: Response): Promise<Response> {
+    //     const hashedPassword = await User.generateHashedPassword(newPassword);
+    //     const existingUser = await this.userDao.findUser({id: userId, passwordResetToken}, false);
+    //
+    //     if (!existingUser) {
+    //         logger.debug("did not find user for id and token");
+    //         return response.status(404).json("user does not exist");
+    //     }
+    //     if (!existingUser.passwordResetIsValid()) {
+    //         logger.debug("user password reset expired");
+    //         return response.status(403).json("expired");
+    //     }
+    //
+    //     await this.userDao.updateUser(userId, {
+    //         password: hashedPassword,
+    //         passwordResetExpiresOn: undefined });
+    //     return response.status(200).json("success");
+    // }
 }
--- tests/unit/api/routes/AuthController.test.ts
+++ tests/unit/api/routes/AuthController.test.ts
@@ -3,42 +3,25 @@ import "jest";
 import "jest-extended";
 import * as routingControllers from "routing-controllers";
 import AuthController from "../../../../src/api/routes/AuthController";
-import { deserializeUser } from "../../../../src/bootstrap/auth";
+import { deserializeUser } from "../../../../src/authentication/auth";
 import UserDAO from "../../../../src/DAO/UserDAO";
 import User from "../../../../src/models/user";
+import {UserFactory} from "../../../factories/UserFactory";
 
 describe("AuthController", () => {
-    let authController: AuthController;
-    let mockReq: Request;
-    let mockRes: Response;
-    let mockSess: any;
     const mockUserDAO = {
-        updateUser: jest.fn(),
-        findUser: jest.fn(),
+        getUserById: jest.fn(),
+        // updateUser: jest.fn(),
+        // findUser: jest.fn(),
     };
-
-    beforeAll(() => {
-        // @ts-ignore
-        routingControllers.Req = jest.fn();
-        // @ts-ignore
-        deserializeUser = jest.fn((userId: number) =>
-            new User({ id: userId, email: "example@test.com", password: "test" }));
-        // @ts-ignore
-        mockReq = { session: {} };
-        mockSess = { user: 1 };
-        // @ts-ignore
-        mockRes = {
-            status: jest.fn(function() {
-                // @ts-ignore
-                return this;
-            }),
-            json: jest.fn(function() {
-                // @ts-ignore
-                return this;
-            }),
-        };
-        authController = new AuthController(mockUserDAO as unknown as UserDAO);
-    });
+    const authController: AuthController = new AuthController(mockUserDAO as unknown as UserDAO);
+    // @ts-ignore
+    const mockReq: Request = { session: {} };
+    // const mockRes: Response = ;
+    // @ts-ignore
+    const mockSess = { user: 1 };
+    const testUser = UserFactory.getUser("j@gm.com", "Jatheesh", undefined, undefined, {id: "d4e3fe52-1b18-4cb6-96b1-600ed86ec45b"});
+    const testUserModel = testUser.toUserModel();
 
     afterEach(() => {
         Object.entries(mockUserDAO).forEach((kvp: [string, jest.Mock<any, any>]) => {
@@ -47,90 +30,90 @@ describe("AuthController", () => {
     });
 
     describe("login method", () => {
-        it("should return the public user that logged in", async () => {
-            const expectedUser = new User({id: 1, email: "example@test.com", password: "test"});
+        it("should return the user model that logged in", async () => {
+            mockUserDAO.getUserById.mockReturnValueOnce(testUserModel);
             const res = await authController.login(mockReq, mockSess);
-            expect(res).toEqual(expectedUser.publicUser);
+            expect(res).toEqual(testUserModel);
         });
     });
 
     describe("signup method", () => {
-        it("should return the public user that signed in", async () => {
-            const expectedUser = new User({id: 1, email: "example@test.com", password: "test"});
+        it("should return the user model that signed in", async () => {
+            mockUserDAO.getUserById.mockReturnValueOnce(testUserModel);
             const res = await authController.signup(mockReq, mockSess);
-            expect(res).toEqual(expectedUser.publicUser);
+            expect(res).toEqual(testUserModel);
         });
     });
-
-    describe("logout method", () => {
-        it("should resolve the promise and destroy the session if logout is successful", async () => {
-            mockReq.session!.destroy = jest.fn(cb => {
-                return cb(undefined);
-            });
-            expect(mockSess).toEqual({ user: 1 });
-            const res = authController.logout(mockReq, mockSess);
-            await expect(res).resolves.toBeTrue();
-            expect(mockReq.session!.destroy).toHaveBeenCalledTimes(1);
-            expect(mockSess).toBeEmpty();
-            expect(mockUserDAO.updateUser).toHaveBeenCalledTimes(1);
-            expect(mockUserDAO.updateUser).toHaveBeenCalledWith(1, {lastLoggedIn: expect.toBeDate()});
-        });
-        it("should resolve the promise if there is no userId on the session", async () => {
-            mockSess = {};
-            mockReq.session!.destroy = jest.fn(cb => {
-                return cb(undefined);
-            });
-            const res = authController.logout(mockReq, mockSess);
-            await expect(res).resolves.toBeTrue();
-            expect(mockReq.session!.destroy).toHaveBeenCalledTimes(0);
-            expect(mockUserDAO.updateUser).toHaveBeenCalledTimes(0);
-            // Reset mockSess for following tests
-            mockSess = { user: 1 };
-        });
-        it("should reject the promise if destroying the request session fails somehow", async () => {
-            const err = new Error("Failed to destroy request session");
-            mockReq.session!.destroy = jest.fn(cb => {
-                return cb(err);
-            });
-            const res = authController.logout(mockReq, mockSess);
-            await expect(res).rejects.toThrow(err);
-            expect(mockUserDAO.updateUser).toHaveBeenCalledTimes(0);
-        });
-    });
-
-    describe("resetPassword method", () => {
-        it("should return a successful response and update user", async () => {
-            const date = new Date(Date.now() + (30 * 60 * 1000)); // half an hour from now
-            mockUserDAO.findUser.mockResolvedValueOnce(new User({id: 1, passwordResetExpiresOn: date}));
-            await authController.resetPassword(1, "lol", "xyz-uuid", mockRes);
-            const expectedUserObj = {password: expect.toBeString(),
-                passwordResetExpiresOn: undefined, passwordResetToken: undefined};
-            expect(mockUserDAO.findUser).toHaveBeenCalledTimes(1);
-            expect(mockUserDAO.findUser).toHaveBeenCalledWith({id: 1, passwordResetToken: "xyz-uuid"}, false);
-            expect(mockUserDAO.updateUser).toHaveBeenCalledTimes(1);
-            expect(mockUserDAO.updateUser).toHaveBeenCalledWith(1, expectedUserObj);
-            expect(mockRes.status).toHaveBeenCalledWith(200);
-            expect(mockRes.json).toHaveBeenCalledWith("success");
-        });
-
-        it("should return a 404 Not Found status if the user with that ID/password token don't exist", async () => {
-            // Actually I think the DAO function will just fail anyway
-            mockUserDAO.findUser.mockResolvedValueOnce(undefined);
-            await authController.resetPassword(1, "lol", "not-found-uuid", mockRes);
-            expect(mockUserDAO.findUser).toHaveBeenCalledTimes(1);
-            expect(mockUserDAO.findUser).toHaveBeenCalledWith({id: 1, passwordResetToken: "not-found-uuid"}, false);
-            expect(mockUserDAO.updateUser).toHaveBeenCalledTimes(0);
-            expect(mockRes.status).toHaveBeenCalledWith(404);
-        });
-
-        it("should return a 403 Forbidden status if the user's reset token has expired", async () => {
-            const date = new Date("January 1 1990");
-            mockUserDAO.findUser.mockResolvedValueOnce(new User({id: 1, passwordResetExpiresOn: date}));
-            await authController.resetPassword(1, "lol", "xyz-uuid", mockRes);
-            expect(mockUserDAO.findUser).toHaveBeenCalledTimes(1);
-            expect(mockUserDAO.findUser).toHaveBeenCalledWith({id: 1, passwordResetToken: "xyz-uuid"}, false);
-            expect(mockUserDAO.updateUser).toHaveBeenCalledTimes(0);
-            expect(mockRes.status).toHaveBeenCalledWith(403);
-        });
-    });
+    //
+    // describe("logout method", () => {
+    //     it("should resolve the promise and destroy the session if logout is successful", async () => {
+    //         mockReq.session!.destroy = jest.fn(cb => {
+    //             return cb(undefined);
+    //         });
+    //         expect(mockSess).toEqual({ user: 1 });
+    //         const res = authController.logout(mockReq, mockSess);
+    //         await expect(res).resolves.toBeTrue();
+    //         expect(mockReq.session!.destroy).toHaveBeenCalledTimes(1);
+    //         expect(mockSess).toBeEmpty();
+    //         expect(mockUserDAO.updateUser).toHaveBeenCalledTimes(1);
+    //         expect(mockUserDAO.updateUser).toHaveBeenCalledWith(1, {lastLoggedIn: expect.toBeDate()});
+    //     });
+    //     it("should resolve the promise if there is no userId on the session", async () => {
+    //         mockSess = {};
+    //         mockReq.session!.destroy = jest.fn(cb => {
+    //             return cb(undefined);
+    //         });
+    //         const res = authController.logout(mockReq, mockSess);
+    //         await expect(res).resolves.toBeTrue();
+    //         expect(mockReq.session!.destroy).toHaveBeenCalledTimes(0);
+    //         expect(mockUserDAO.updateUser).toHaveBeenCalledTimes(0);
+    //         // Reset mockSess for following tests
+    //         mockSess = { user: 1 };
+    //     });
+    //     it("should reject the promise if destroying the request session fails somehow", async () => {
+    //         const err = new Error("Failed to destroy request session");
+    //         mockReq.session!.destroy = jest.fn(cb => {
+    //             return cb(err);
+    //         });
+    //         const res = authController.logout(mockReq, mockSess);
+    //         await expect(res).rejects.toThrow(err);
+    //         expect(mockUserDAO.updateUser).toHaveBeenCalledTimes(0);
+    //     });
+    // });
+    //
+    // describe("resetPassword method", () => {
+    //     it("should return a successful response and update user", async () => {
+    //         const date = new Date(Date.now() + (30 * 60 * 1000)); // half an hour from now
+    //         mockUserDAO.findUser.mockResolvedValueOnce(new User({id: 1, passwordResetExpiresOn: date}));
+    //         await authController.resetPassword(1, "lol", "xyz-uuid", mockRes);
+    //         const expectedUserObj = {password: expect.toBeString(),
+    //             passwordResetExpiresOn: undefined, passwordResetToken: undefined};
+    //         expect(mockUserDAO.findUser).toHaveBeenCalledTimes(1);
+    //         expect(mockUserDAO.findUser).toHaveBeenCalledWith({id: 1, passwordResetToken: "xyz-uuid"}, false);
+    //         expect(mockUserDAO.updateUser).toHaveBeenCalledTimes(1);
+    //         expect(mockUserDAO.updateUser).toHaveBeenCalledWith(1, expectedUserObj);
+    //         expect(mockRes.status).toHaveBeenCalledWith(200);
+    //         expect(mockRes.json).toHaveBeenCalledWith("success");
+    //     });
+    //
+    //     it("should return a 404 Not Found status if the user with that ID/password token don't exist", async () => {
+    //         // Actually I think the DAO function will just fail anyway
+    //         mockUserDAO.findUser.mockResolvedValueOnce(undefined);
+    //         await authController.resetPassword(1, "lol", "not-found-uuid", mockRes);
+    //         expect(mockUserDAO.findUser).toHaveBeenCalledTimes(1);
+    //         expect(mockUserDAO.findUser).toHaveBeenCalledWith({id: 1, passwordResetToken: "not-found-uuid"}, false);
+    //         expect(mockUserDAO.updateUser).toHaveBeenCalledTimes(0);
+    //         expect(mockRes.status).toHaveBeenCalledWith(404);
+    //     });
+    //
+    //     it("should return a 403 Forbidden status if the user's reset token has expired", async () => {
+    //         const date = new Date("January 1 1990");
+    //         mockUserDAO.findUser.mockResolvedValueOnce(new User({id: 1, passwordResetExpiresOn: date}));
+    //         await authController.resetPassword(1, "lol", "xyz-uuid", mockRes);
+    //         expect(mockUserDAO.findUser).toHaveBeenCalledTimes(1);
+    //         expect(mockUserDAO.findUser).toHaveBeenCalledWith({id: 1, passwordResetToken: "xyz-uuid"}, false);
+    //         expect(mockUserDAO.updateUser).toHaveBeenCalledTimes(0);
+    //         expect(mockRes.status).toHaveBeenCalledWith(403);
+    //     });
+    // });
 });
