51b8c58541d7c5a08c8bc737f46bf5f69b14b027
Akosua Asante <akosuaasante@gmail.com>
2019-11-30 12:40:09 -0500

    auth refactor - signupauthentication

--- tests/unit/auth.test.ts
+++ tests/unit/auth.test.ts
@@ -7,87 +7,78 @@ import { ConflictError } from "../../src/api/middlewares/ErrorHandler";
 import {
     authorizationChecker,
     currentUserChecker, deserializeUser, generateHashedPassword, serializeUser,
-    signInAuthentication,
+    signInAuthentication, signUpAuthentication,
 
 } from "../../src/bootstrap/auth";
 import UserDO, { Role } from "../../src/models/user";
 import { User } from "@akosasante/trade-machine-models";
 import {UserFactory} from "../factories/UserFactory";
+import UserDAO from "../../src/DAO/UserDAO";
 
 const testUser = UserFactory.getUser("j@gm.com", "Jatheesh", undefined, undefined, {id: "d4e3fe52-1b18-4cb6-96b1-600ed86ec45b"});
 const testUserModel = testUser.toUserModel();
+const mockUserDAO = {
+    // getAllUsers: jest.fn(),
+    // getAllUsersWithTeams: jest.fn(),
+    getUserById: jest.fn(),
+    findUser: jest.fn(),
+    // findUsers: jest.fn(),
+    createUsers: jest.fn(),
+    updateUser: jest.fn(),
+    // deleteUser: jest.fn(),
+};
 
 describe("Authorization helper methods", () => {
     describe("serializeUser", () => {
         it("should return the user ID", async () => {
             const id = await serializeUser(testUserModel);
+            
             expect(id).toBeString();
             expect(id).toEqual(testUserModel.id);
         });
     });
-    //
-    // describe("deserializeUser", () => {
-    //     it("should return the user from a given ID", async () => {
-    //         const user = await deserializeUser(1);
-    //         expect(user!.equals(testUser)).toBeTrue();
-    //     });
-    //     it("should throw an error if the user ID is not found", async () => {
-    //         mockUserDb.findOneOrFail = jest.fn(id => {
-    //             throw new EntityNotFoundError("User", `No user found with this ${id}`);
-    //         });
-    //         const user = deserializeUser(200);
-    //         await expect(user).rejects.toThrow(EntityNotFoundError);
-    //     });
-    // });
-    //
-    // describe("signUpAuthentication", () => {
-    //     const email = "test@example.com";
-    //     const expectedUser = new User({
-    //         email,
-    //         password: testUser.password!,
-    //         lastLoggedIn: new Date(),
-    //     });
-    //     it("should create and return a new user if none existed before", async () => {
-    //         const cb = jest.fn();
-    //         mockUserDb.findOne = jest.fn(id => undefined);
-    //         mockUserDb.save = jest.fn(userObj => expectedUser);
-    //         generateHashedPassword = jest.fn();
-    //
-    //         await signUpAuthentication(email, testUser.password!, cb);
-    //         expect(mockUserDb.save).toBeCalledTimes(1);
-    //         expect(cb).toBeCalledTimes(1);
-    //         expect(cb).toBeCalledWith(undefined, expectedUser);
-    //     });
-    //     it("should update and return an existing user with no password", async () => {
-    //         const cb = jest.fn();
-    //         mockUserDb.findOne = jest.fn(id => (new User({id: 1, email})));
-    //         mockUserDb.findOneOrFail = jest.fn((id, userObj) => expectedUser);
-    //         generateHashedPassword = jest.fn();
-    //
-    //         await signUpAuthentication(email, testUser.password!, cb);
-    //         expect(mockUserDb.update).toBeCalledTimes(1);
-    //         expect(cb).toBeCalledTimes(1);
-    //         expect(cb).toBeCalledWith(undefined, expectedUser);
-    //     });
-    //     it("should return a ConflictError if the player is already signed up", async () => {
-    //         const cb = jest.fn();
-    //         mockUserDb.findOne = jest.fn(id => testUser);
-    //
-    //         await signUpAuthentication(email, testUser.password!, cb);
-    //         expect(cb).toBeCalledTimes(1);
-    //         expect(cb).toBeCalledWith(new ConflictError("Email already in use and signed up."));
-    //     });
-    //     it("should return any other errors encountered", async () => {
-    //         const cb = jest.fn();
-    //         mockUserDb.findOne.mockImplementationOnce(() => {
-    //             throw new Error("Error while fetching user");
-    //         });
-    //
-    //         await signUpAuthentication(email, testUser.password!, cb);
-    //         expect(cb).toHaveBeenCalledTimes(1);
-    //         expect(cb).toHaveBeenCalledWith(new Error("Error while fetching user"));
-    //     });
-    // });
+
+    describe("deserializeUser", () => {
+        it("should return the user from a given ID", async () => {
+            mockUserDAO.getUserById.mockReturnValueOnce(testUserModel);
+            const user = await deserializeUser("uuid", mockUserDAO as unknown as UserDAO);
+            
+            expect(user).toEqual(testUserModel);
+        });
+    });
+
+    describe("signUpAuthentication", () => {
+        const cb = jest.fn();
+        afterEach(() => {
+            cb.mockClear();
+        });
+
+        it("should create and return a new user if none existed before", async () => {
+            mockUserDAO.findUser.mockReturnValueOnce(undefined);
+            mockUserDAO.createUsers.mockReturnValueOnce([testUserModel]);
+            await signUpAuthentication(testUser.email!, testUser.password!, mockUserDAO as unknown as UserDAO, cb);
+            
+            expect(cb).toBeCalledTimes(1);
+            expect(cb).toBeCalledWith(undefined, testUserModel);
+        });
+        it("should update and return an existing user with no password", async () => {
+            const passwordlessUser = new UserDO({...testUser, password: undefined});
+            const passwordlessUserModel = passwordlessUser.toUserModel();
+            mockUserDAO.findUser.mockReturnValueOnce(passwordlessUserModel);
+            mockUserDAO.updateUser.mockReturnValueOnce(testUserModel);
+            await signUpAuthentication(testUser.email!, testUser.password!, mockUserDAO as unknown as UserDAO, cb);
+            
+            expect(cb).toBeCalledTimes(1);
+            expect(cb).toBeCalledWith(undefined, testUserModel);
+        });
+        it("should return a ConflictError if the player is already signed up", async () => {
+            mockUserDAO.findUser.mockReturnValueOnce(testUserModel);
+            await signUpAuthentication(testUser.email!, testUser.password!, mockUserDAO as unknown as UserDAO, cb);
+            
+            expect(cb).toBeCalledTimes(1);
+            expect(cb).toBeCalledWith(new ConflictError("Email already in use and signed up."));
+        });
+    });
     //
     // describe("signInAuthentication", () => {
     //     const email = "test@example.com";
