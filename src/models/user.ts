71ba2a8c2e7ce0ff419ddc2de18b99ed747f6de4
Akosua Asante <akosuaasante@gmail.com>
2020-03-08 00:09:24 -0500

    update user entity

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,7 +1,6 @@
-import { User } from "@akosasante/trade-machine-models";
 import { Column, Entity, Index, ManyToOne, OneToMany, Unique } from "typeorm";
 import logger from "../bootstrap/logger";
-import { BaseModel, Excludes, HasEquals } from "./base";
+import { BaseModel } from "./base";
 import GeneralSettings from "./generalSettings";
 import ScheduledDowntime from "./scheduledDowntime";
 import Team from "./team";
@@ -18,20 +17,10 @@ export enum UserStatus {
 
 @Entity({name: "user"})
 @Unique(["email"])
-export default class UserDO extends BaseModel implements HasEquals {
-    // public static generateTimeToPasswordExpires() {
-    //     return new Date(Date.now() + User.TIME_TO_EXPIRE_PASSWORD_MS);
-    // }
-    //
-    // public static sanitizeUUID(uuid: string) {
-    //     return uuid.replace(/-/g, "");
-    // }
-    //
-    // private static TIME_TO_EXPIRE_PASSWORD_MS = 1 * 60 * 60 * 1000;  // 1 hr * 60 min/hr * 60 s/min * 1000ms/s
+export default class User extends BaseModel {
 
     @Column()
-    @Index({unique: true})
-    public email?: string;
+    public email!: string;
 
     @Column({nullable: true})
     public password?: string;
@@ -57,84 +46,24 @@ export default class UserDO extends BaseModel implements HasEquals {
     @Column({ type: "enum", enum: UserStatus, default: UserStatus.ACTIVE })
     public status?: UserStatus;
 
-    @ManyToOne(type => Team, team => team.owners, {onDelete: "SET NULL"})
-    public team?: Team;
+    // @ManyToOne(type => Team, team => team.owners, {onDelete: "SET NULL"})
+    // public team?: Team;
+    //
+    // @OneToMany(type => ScheduledDowntime, schedule => schedule.createdBy)
+    // public createdSchedules?: ScheduledDowntime[];
+    //
+    // @OneToMany(type => ScheduledDowntime, schedule => schedule.modifiedBy)
+    // public updatedSchedules?: ScheduledDowntime[];
+    //
+    // @OneToMany(type => GeneralSettings, setting => setting.modifiedBy)
+    // public updatedSettings?: GeneralSettings[];
 
-    @OneToMany(type => ScheduledDowntime, schedule => schedule.createdBy)
-    public createdSchedules?: ScheduledDowntime[];
-
-    @OneToMany(type => ScheduledDowntime, schedule => schedule.modifiedBy)
-    public updatedSchedules?: ScheduledDowntime[];
-
-    @OneToMany(type => GeneralSettings, setting => setting.modifiedBy)
-    public updatedSettings?: GeneralSettings[];
-
-    constructor(userObj: Partial<UserDO> = {}) {
-        super(userObj.id);
-        this.password = userObj.password;
-        this.displayName = userObj.displayName;
-        this.email = userObj.email;
-        this.role = userObj.role;
-        this.status = userObj.status;
-        this.slackUsername = userObj.slackUsername;
-        this.lastLoggedIn = userObj.lastLoggedIn;
-        this.dateCreated = userObj.dateCreated;
-        this.dateModified = userObj.dateModified;
-        // this.hasPassword = !!this.password;
-        // this.userIdToken = userObj.userIdToken;
-        this.passwordResetExpiresOn = userObj.passwordResetExpiresOn;
-        this.passwordResetToken = userObj.passwordResetToken;
-        this.team = userObj.team;
-        this.createdSchedules = userObj.createdSchedules;
-        this.updatedSchedules = userObj.updatedSchedules;
-        this.updatedSettings = userObj.updatedSettings;
-    }
-
-    public toUserModel(): User {
-        if (this.id && this.displayName && this.email) {
-            return new User(
-                this.id,
-                !!this.password,
-                this.displayName,
-                this.email,
-                this.role,
-                this.status,
-                this.slackUsername,
-                this.lastLoggedIn,
-                this.team ? this.team.toTeamModel() : undefined
-            );
-        } else {
-            throw new Error("Invalid User Model inputs");
-        }
-    }
-
-    public equals(other: UserDO, excludes?: Excludes, bypassDefaults: boolean = false): boolean {
-        logger.debug("User equals check");
-        const COMPLEX_FIELDS = {roles: true};
-        const MODEL_FIELDS = {
-            team: true,
-            createdSchedules: true,
-            updatedSchedules: true,
-            updatedSettings: true,
-        };
-        const DEFAULT_EXCLUDES = {
-            id: true,
-            password: true,
-            userIdToken: true,
-            lastLoggedIn: true,
-            dateCreated: true,
-            dateModified: true,
-            createdSchedules: true,
-            updatedSchedules: true,
-            updatedSettings: true,
-            passwordResetExpiresOn: true,
-            passwordResetToken: true,
-        };
-        excludes = bypassDefaults ? excludes : Object.assign(DEFAULT_EXCLUDES, (excludes || {}));
-        return BaseModel.equals(this, other, excludes, COMPLEX_FIELDS, MODEL_FIELDS);
+    constructor(props: Partial<User> & Required<Pick<User, "email">>) {
+        super();
+        Object.assign(this, props);
     }
 
     public isAdmin() {
-        return this.toUserModel().isAdmin();
+        return this.role === Role.ADMIN;
     }
 }
