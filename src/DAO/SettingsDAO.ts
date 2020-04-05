cb197a27d61788aff2af44cf1c2b295efae20ce9
Akosua Asante <akosuaasante@gmail.com>
2020-04-05 03:35:43 -0400

    update settings dao

--- src/DAO/SettingsDAO.ts
+++ src/DAO/SettingsDAO.ts
@@ -1,99 +1,34 @@
-import { NotFoundError } from "routing-controllers";
-import { Connection, DeleteResult, FindManyOptions, getConnection, LessThan, MoreThan, Repository } from "typeorm";
-import GeneralSettings from "../models/generalSettings";
-import ScheduledDowntime from "../models/scheduledDowntime";
-
-export enum ScheduleGetAllOptions {
-    PREVIOUS,
-    FUTURE,
-}
+import { FindManyOptions, FindOneOptions, getConnection, InsertResult, Repository } from "typeorm";
+import Settings from "../models/settings";
 
 export default class SettingsDAO {
-    public connection: Connection;
-    private scheduleDb: Repository<ScheduledDowntime>;
-    private settingsDb: Repository<GeneralSettings>;
+    private settingsDb: Repository<Settings>;
 
-    constructor() {
-        this.connection = getConnection(process.env.NODE_ENV);
-        this.scheduleDb = this.connection.getRepository("ScheduledDowntime");
-        this.settingsDb = this.connection.getRepository("GeneralSettings");
+    constructor(repo?: Repository<Settings>) {
+        this.settingsDb = repo || getConnection(process.env.NODE_ENV).getRepository("Settings");
     }
 
-    public async getAllScheduledDowntimes(option?: ScheduleGetAllOptions): Promise<ScheduledDowntime[]> {
-        // @ts-ignore
-        const queryOpts: FindManyOptions<ScheduledDowntime> = { order: { startTime: "ASC" } };
-        if (option !== undefined && Object.values(ScheduleGetAllOptions).includes(option)) {
-            queryOpts.where = option === ScheduleGetAllOptions.FUTURE
-                ? { startTime: MoreThan(new Date()) }
-                : { endTime: LessThan(new Date()) };
-        }
-        const dbSchedules = await this.scheduleDb.find(queryOpts);
-        return dbSchedules.map(sch => new ScheduledDowntime(sch));
+    public async getAllSettings(): Promise<Settings[]> {
+        const options: FindManyOptions = {order: {dateCreated: "DESC"}};
+        return await this.settingsDb.find(options);
     }
 
-    public async getScheduledDowntimeById(id: number): Promise<ScheduledDowntime> {
-        if (!id) {
-            throw new NotFoundError("Id is required");
-        }
-        const dbSchedule = await this.scheduleDb.findOneOrFail(id);
-        return new ScheduledDowntime(dbSchedule);
+    public async getMostRecentSettings(): Promise<Settings | undefined> {
+        const options: FindOneOptions = {order: {dateCreated: "DESC"}};
+        return await this.settingsDb.findOne(options);
     }
 
-    public async getCurrentlyScheduledDowntime(): Promise<ScheduledDowntime> {
-        const queryOpts: FindManyOptions<ScheduledDowntime> = {
-            where: { startTime: LessThan(new Date()), endTime: MoreThan(new Date()) } ,
-        };
-        const dbSchedule = await this.scheduleDb.findOneOrFail(queryOpts);
-        return new ScheduledDowntime(dbSchedule);
+    public async getSettingsById(id: string): Promise<Settings> {
+        return await this.settingsDb.findOneOrFail(id);
     }
 
-    public async createScheduledDowntime(downtimeObj: Partial<ScheduledDowntime>): Promise<ScheduledDowntime> {
-        const dbSchedule = await this.scheduleDb.save(downtimeObj);
-        return new ScheduledDowntime(dbSchedule);
-    }
-
-    public async updateScheduledDowntime(id: number, downtimeObj: Partial<ScheduledDowntime>):
-        Promise<ScheduledDowntime> {
-        await this.scheduleDb.update( { id }, downtimeObj);
-        return await this.getScheduledDowntimeById(id);
-    }
-
-    public async deleteScheduledDowntime(id: number): Promise<DeleteResult> {
-        if (!id) {
-            throw new NotFoundError("Id is required");
-        }
-        await this.scheduleDb.findOneOrFail(id);
-        return await this.scheduleDb.createQueryBuilder()
-            .delete()
-            .whereInIds(id)
-            .returning("id")
-            .execute();
-    }
-
-    public async getAllGeneralSettings(): Promise<GeneralSettings[]> {
-        const options: FindManyOptions = {order: {dateCreated: "DESC"}, relations: ["modifiedBy"]};
-        const dbSettings = await this.settingsDb.find(options);
-        return dbSettings.map(settings => new GeneralSettings(settings));
-    }
-
-    public async getSettingsById(id: number): Promise<GeneralSettings> {
-        if (!id) {
-            throw new NotFoundError("Id is required");
-        }
-        const dbSettings = await this.settingsDb.findOneOrFail(id, {relations: ["modifiedBy"]});
-        return new GeneralSettings(dbSettings);
-    }
-
-    public async getMostRecentSettings(): Promise<GeneralSettings|undefined> {
-        const options: FindManyOptions = {order: {dateCreated: "DESC"}, take: 1, relations: ["modifiedBy"]};
-        const dbSettings = await this.settingsDb.find(options);
-        return dbSettings.length ? new GeneralSettings(dbSettings[0]) : undefined;
-    }
-
-    public async insertNewSettingsLine(settingsObj: Partial<GeneralSettings>): Promise<GeneralSettings> {
+    public async insertNewSettings(settings: Partial<Settings>): Promise<Settings | undefined> {
         const mostRecentSettings = await this.getMostRecentSettings();
-        const newSettingsObj = Object.assign((mostRecentSettings || {}), settingsObj, {id: undefined});
-        const dbSettings = await this.settingsDb.save(newSettingsObj);
-        return new GeneralSettings(dbSettings);
+        const newLine = {...(mostRecentSettings || {}),
+            ...settings, id: undefined, dateModified: undefined, dateCreated: undefined,
+        };
+        const result: InsertResult =  await this.settingsDb.insert(newLine);
+        const retrievedLine =  await this.settingsDb.find(result.identifiers[0]);
+        return retrievedLine && retrievedLine.length ? retrievedLine[0] : undefined;
     }
 }
