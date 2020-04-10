1f8c820df72ef7a143bee09b684747b4771a26ab
Akosua Asante <akosuaasante@gmail.com>
2020-04-10 11:38:02 -0400

    update jest

--- jest.config.js
+++ jest.config.js
@@ -87,7 +87,10 @@ module.exports = {
     ],
 
     // A map from regular expressions to module names that allow to stub out resources with a single module
-    // moduleNameMapper: {},
+    moduleNameMapper: {
+        // '\*': '<rootDir>/node_modules/$1',
+        '^@/(.*)$': '<rootDir>/src/$1'
+    },
 
     // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
     // modulePathIgnorePatterns: [],
