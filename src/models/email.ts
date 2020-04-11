2246387417ba6133ffb4d284e1ea96884fd165b7
Akosua Asante <akosuaasante@gmail.com>
2020-04-11 01:15:53 -0400

    new email model and test

---
+++ src/models/email.ts
@@ -0,0 +1,29 @@
+import {Column, CreateDateColumn, Entity, Index, PrimaryColumn, UpdateDateColumn} from "typeorm";
+
+@Entity()
+export default class Email {
+    @PrimaryColumn()
+    public messageId!: string;
+
+    @Column()
+    @Index()
+    public status?: string;
+
+    @CreateDateColumn()
+    public dateCreated?: Date;
+
+    @UpdateDateColumn()
+    public dateModified?: Date;
+
+    public toString(): string {
+        return `${this.constructor.name}#${this.messageId}`;
+    }
+
+    public parse<T extends Email>(): Partial<T> {
+        return Object.assign({}, this);
+    }
+
+    constructor(props: Partial<Email> & Required<Pick<Email, "messageId">>) {
+        Object.assign(this, props);
+    }
+}
