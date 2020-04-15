0a154ec3c5dc8b71ce9ef98da26b2137b01db509
Akosua Asante <akosuaasante@gmail.com>
2020-04-15 14:31:38 -0400

    ESPN Api expansion and updates

--- src/espn/espnApi.ts
+++ src/espn/espnApi.ts
@@ -1,5 +1,4 @@
 import axios, { AxiosInstance } from "axios";
-import { NotFoundError } from "routing-controllers";
 
 interface EspnLeagueMember {
     id: string;
@@ -7,13 +6,119 @@ interface EspnLeagueMember {
     displayName: string;
 }
 
+interface EspnRecord {
+    gamesBack: number;
+    losses: number;
+    percentage: number;
+    pointsAgainst: number;
+    pointsFor: number;
+    streakLength: number;
+    streakType: "WIN"|"LOSS"|string;
+    ties: number;
+    wins: number;
+}
+
+interface EspnRecordObj {
+    away: EspnRecord;
+    home: EspnRecord;
+    division: EspnRecord;
+    overall: EspnRecord;
+}
+
 interface EspnFantasyTeam {
     id: number;
-    abbrev: string;
-    location: string;
-    nickname: string;
-    owners: string[];
+    abbrev?: string;
+    location?: string;
+    nickname?: string;
+    owners?: string[];
+    divisionId?: number;
+    isActive?: boolean;
+    logo?: string;
+    record?: EspnRecordObj;
+    playoffSeed?: number;
+    rankFinal?: number;
+    rankCalculatedFinal?: number;
+    points?: number;
+    pointsAdjusted?: number;
+    pointsDelta?: number;
 }
+
+interface EspnPlayerInfo {
+    id: number;
+    firstName?: string;
+    lastName?: string;
+    fullName?: string;
+    proTeamId?: number;
+    eligibleSlots?: number[]; // TODO: figure out what this maps to
+    defaultPositionId?: number; // TODO: Map position number to position text
+    ownership?: object;
+    jersey?: string;
+    injured?: boolean;
+    injuryStatus?: string;
+    active?: boolean;
+
+}
+
+interface EspnPlayerPoolEntry {
+    player: EspnPlayerInfo;
+    keeperValue: number;
+    keeperValueFuture: number;
+    onTeamId: number;
+    id: number;
+    appliedStatTotal: number;
+    status: string; // "ONTEAM" idk what else
+}
+
+interface EspnOwnedPlayer {
+    lineupSlotId?: number;
+    playerId?: number;
+    status?: string;
+    injuryStatus?: string;
+    playerPoolEntry?: EspnPlayerPoolEntry;
+    acquisitionDate?: any;
+    acquisitionType?: any;
+}
+
+interface EspnMajorLeaguePlayer {
+    id: number;
+    status?: string;
+    onTeamId?: any;
+    player?: EspnPlayerInfo;
+}
+
+interface EspnCumulativeScore {
+    losses?: number;
+    wins?: number;
+    ties?: number;
+    statBySlot?: object;
+    scoreByStat?: object;
+}
+
+interface EspnRoster {
+    entries: EspnPlayerInfo[];
+}
+
+interface EspnScoreObj {
+    teamId?: number;
+    tiebreak?: number;
+    totalPoints?: number;
+    totalPointsLive?: number;
+    adjustment?: number;
+    cumulativeScore?: EspnCumulativeScore;
+    rosterForCurrentScoringPeriod?: EspnRoster;
+    rosterForMatchupPeriod?: EspnRoster;
+}
+
+interface EspnScheduleItem {
+    id: number;
+    winner?: "HOME"|"AWAY"|"UNDECIDED";
+    home?: EspnScoreObj;
+    away?: EspnScoreObj;
+    playoffTierType?: string;
+}
+
+type EspnSchedule = EspnScheduleItem[];
+
 
 export default class EspnAPI {
 
@@ -22,62 +127,56 @@ export default class EspnAPI {
     }
 
     public leagueName: string = "";
-    public teams: EspnFantasyTeam[] = []; // TODO: Refine type
-    public members: EspnLeagueMember[] = []; // TODO: Refine type
-    public leagueBaseUrl: string;
+    public teams: EspnFantasyTeam[] = [];
+    public members: EspnLeagueMember[] = [];
 
     private leagueId: number;
     private req: AxiosInstance;
-    private loaded: boolean = false;
-    // tslint:disable-next-line:max-line-length
-    private BASE_URL: string = "http://fantasy.espn.com/apis/v3/games/flb/seasons/2019/segments/0"; // TODO: Deal with season year changing
-    // tslint:disable-next-line:max-line-length
-    private ESPNS2_COOKIE: string = "AEAKC%2BYZ5pDnXWWN3QjnK8pizxJbwojo%2FBzaQxaiZPjx9SdB%2F7cB%2BxrCNk%2Fk0iIzpSQLm6OsH%2BIrwfcdeJ19Y3lqjRDn1hV9%2FEGHVX6ZRGpuYjKeeowwIOr8YHlX4C6zkPk0TP1D4PInK6cEpudD80A8fxwbicDRkNZd929xntcmetjihDfPn2C70AhqZUxkOmf4P2dwjogjkZmFN3zgjCP8yfWm521yiuWR9MGaksnzv2aLpMEb36IsGZJSLbqHYfmv1QTJrjXi1nzcYHpEvfo6";
-    private ESPN_SWID: string = "{D83BF7DC-2473-4A49-B53D-00000D9FF850}";
+    private ESPNS2_COOKIE = process.env.ESPN_COOKIE;
+    private ESPN_SWID = process.env.ESPN_SWID;
+    private static getBaseUrl(season: number = 2019, leagueId: number): string {
+        if (season >= 2017) {
+            return `http://fantasy.espn.com/apis/v3/games/flb/seasons/${season}/segments/0/leagues/${leagueId}`;
+        } else {
+            return `https://fantasy.espn.com/apis/v3/games/ffl/leagueHistory/${leagueId}?seasonId=${season}`;
+        }
+    }
 
     constructor(leagueId: number) {
         this.leagueId = leagueId;
-        this.leagueBaseUrl = `${this.BASE_URL}/leagues/${this.leagueId}`;
         this.req = axios.create({
             withCredentials: true,
             headers: {Cookie: `espn_s2=${this.ESPNS2_COOKIE}; SWID=${this.ESPN_SWID};`},
         });
     }
 
-    public async loadAndRun(func: (..._: any[]) => any) {
-        if (!this.loaded) {
-            await this.preloadData();
-        }
-        return func();
+    public async getAllLeagueData(year: number) {
+        const { data } = await this.req.get(`${EspnAPI.getBaseUrl(year, this.leagueId)}`);
+        return data;
     }
 
-    public async preloadData() {
-        const {data: res} = await this.req.get(`${this.leagueBaseUrl}` );
-        this.members = res.members;
-        this.teams = res.teams;
-        this.leagueName = res.settings.name;
-        this.loaded = true;
-        return res;
+    public async getAllMembers(year: number) {
+        const { data: members } = await this.req.get(`${EspnAPI.getBaseUrl(year, this.leagueId)}/members`);
+        return members;
     }
 
-    public getTeamById(id: number): EspnFantasyTeam|undefined {
-        const foundTeam = this.teams.find(team => team.id === id);
-        if (foundTeam) {
-            return foundTeam;
-        } else {
-            throw new NotFoundError(`No team with that ID found for League#${this.leagueId}`);
-        }
+    public async getAllLeagueTeams(year: number) {
+        const { data: teams } = await this.req.get(`${EspnAPI.getBaseUrl(year, this.leagueId)}/teams?view=mTeam`);
+        return teams;
     }
 
-    public getEspnTeamOwners(team: EspnFantasyTeam): EspnLeagueMember[] {
-        return team.owners
-            .map(id => this.members.find(member => member.id === id))
-            .filter(m => m !== undefined) as EspnLeagueMember[];
+    public async getAllMajorLeaguePlayers(year: number) {
+        const { data: {players: players} } = await this.req.get(`${EspnAPI.getBaseUrl(year, this.leagueId)}?view=kona_player_info`);
+        return players;
+    }
+
+    public async getScheduleForYear(year: number) {
+        const { data: schedule } = await this.req.get(`${EspnAPI.getBaseUrl(year, this.leagueId)}/schedule?view=mScoreboard`);
+        return schedule;
+    }
+
+    public async getRosterForTeamAndDay(year: number, teamId: number, scoringPeriodId: number) {
+        const { data: {teams: teams} } = await this.req.get(`${EspnAPI.getBaseUrl(year, this.leagueId)}?forTeamId=${teamId}&scoringPeriodId=${scoringPeriodId}&view=mRoster`);
+        return teams[0].roster;
     }
 }
-
-// const x = new EspnAPI(545);
-// x.loadAndRun(() => {
-//     console.log(x.members);
-//     console.log(x.getTeamName(x.getTeamById(20)!));
-// });
