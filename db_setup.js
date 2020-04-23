f5e8f9a5b59d9eefbd635474776733ebb5d615ae
Akosua Asante <akosuaasante@gmail.com>
2020-04-23 17:51:11 -0400

    debug

--- db_setup.js
+++ db_setup.js
@@ -1,15 +1,15 @@
 const { Client } = require('pg')
 const client = new Client()
 
-const dbSetup = "CREATE DATABASE trade_machine OWNER $1"
+const dbSetup = `CREATE DATABASE trade_machine OWNER ${process.env.PG_USER};`
 const uuidSetup = `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`
-const schemaSetup = "CREATE SCHEMA test AUTHORIZATION $1;"
+const schemaSetup = `CREATE SCHEMA test AUTHORIZATION ${process.env.PG_USER};`
 
 client.connect().then(async () => {
   try {
-    await client.query(dbSetup, [process.env.PG_USER]);
+    await client.query(dbSetup);
     await client.query(uuidSetup);
-    await client.query(schemaSetup, [process.env.PG_USER]);
+    await client.query(schemaSetup);
   } catch (e) {
     console.error(`Failed to setup db: ${e.toString()}`)
     console.error(e.stack);
