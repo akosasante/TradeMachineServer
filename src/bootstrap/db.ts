a17fbce8143fd8845158ff05d9e6db18f0033c26
Akosua Asante <akosuaasante@gmail.com>
2020-04-22 23:39:49 -0400

    ALMOST THERE

--- src/bootstrap/db.ts
+++ src/bootstrap/db.ts
@@ -7,17 +7,13 @@ export default async function initializeDb(logQueries: boolean = false) {
     let connection: Connection|undefined;
     try {
         const dbConfigName = process.env.NODE_ENV;
-        logger.debug(`secret: ${process.env.PG_USER}`);
-        logger.debug(`GETTING CONNECTION OPTIONS: ${inspect(dbConfigName)}`);
         const connectionConfig = await getConnectionOptions(dbConfigName);
-        logger.debug(`USING CONNECTION CONFIG: ${inspect(connectionConfig)}`);
         connection = await createConnection({
             ...connectionConfig,
             logger: logQueries ? new CustomQueryLogger(logger) : undefined,
         });
-        logger.debug(`GOT CONNECTION: ${connection}`);
         const pgClient = (await connection.driver.obtainMasterConnection())[0];
-        logger.debug(`OBTAINED MASTER CONNECTION: ${inspect(pgClient)}`);
+
         pgClient.on("error", (dbErr: any) => {
             logger.error(`DBERROR: ${inspect(dbErr)}`);
             setTimeout(async () => {
