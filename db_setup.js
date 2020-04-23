111cba5626856fc04dae438bd53e3fff6985a2a1
Akosua Asante <akosuaasante@gmail.com>
2020-04-23 18:13:08 -0400

    debug

--- db_setup.js
+++ db_setup.js
@@ -1,7 +1,6 @@
 const { Client } = require('pg')
 const client = new Client()
 
-const dbSetup = `CREATE DATABASE trade_machine OWNER ${process.env.PGUSER};`
 const uuidSetup = `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`
 const schemaSetup = `CREATE SCHEMA test AUTHORIZATION ${process.env.PGUSER};`
 
@@ -9,17 +8,11 @@ console.log(dbSetup);
 
 client.connect().then(async () => {
   try {
-    await client.query(dbSetup);
     await client.query(uuidSetup);
     await client.query(schemaSetup);
   } catch (e) {
-    if (e.toString().includes("already exists")) {
-      await client.query(uuidSetup);
-      await client.query(schemaSetup);
-    } else {
-      console.error("Failed to setup db")
-      throw e
-    }
+    console.error("Failed to setup db")
+    throw e
   }
 }).finally(async () => {
   await client.end();
