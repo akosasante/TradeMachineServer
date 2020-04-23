37d1cb878cf8e19be9ac17b612e425541c08a4b2
Akosua Asante <akosuaasante@gmail.com>
2020-04-23 17:37:30 -0400

    debug

---
+++ db_setup.js
@@ -0,0 +1,19 @@
+const { Client } = require('pg')
+const client = new Client()
+
+const dbSetup = "CREATE DATABASE test OWNER $1;"
+const uuidSetup = `CREATE EXTENSION IF NOT EXISTS "uuid-ossp";`
+const schemaSetup = "CREATE SCHEMA test AUTHORIZATION $1;"
+
+client.connect().then(async () => {
+  try {
+    await client.query(dbSetup, process.env.PG_USER);
+    await client.query(uuidSetup);
+    await client.query(schemaSetup, process.env.PG_USER);
+  } catch (e) {
+    console.error(`Failed to setup db: ${e.toString()}`)
+    console.error(e.stack);
+  }
+}).finally(async () => {
+  await client.end();
+})
