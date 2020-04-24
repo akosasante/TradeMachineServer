fb86417e7438346d30545cb751d11facafb92f24
Akosua Asante <akosuaasante@gmail.com>
2020-04-23 20:53:46 -0400

    cleanup the db for prod

--- ci_db_setup.sql
+++
@@ -1,7 +0,0 @@
-CREATE USER :user with password ':password';
-CREATE DATABASE :db_name owner :user;
--- SELECT * FROM pg_available_extensions;
-\connect :db_name;
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
--- \df
-CREATE SCHEMA test AUTHORIZATION :user;
