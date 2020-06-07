fe347787631150486e4e4ffbfec2133421642182
Akosua Asante <akosuaasante@gmail.com>
2020-06-07 19:25:23 -0400

    extract methods to tradeFormatter and test

---
+++ src/slack/tradeFormatter.ts
@@ -0,0 +1,117 @@
+import TradeItem from "../models/tradeItem";
+import PlayerDAO from "../DAO/PlayerDAO";
+import Player, { PlayerLeagueType } from "../models/player";
+import DraftPickDAO from "../DAO/DraftPickDAO";
+import DraftPick from "../models/draftPick";
+import ordinal from "ordinal";
+import Trade from "../models/trade";
+import TradeParticipant from "../models/tradeParticipant";
+import logger from "../bootstrap/logger";
+import Team from "../models/team";
+import User from "../models/user";
+import { partition, zip } from "lodash";
+import {inspect} from "util";
+
+interface TradeFormatterDeps {
+    playerDao: PlayerDAO;
+    pickDao: DraftPickDAO;
+}
+
+const TradeFormatter = {
+    async prepPlayerText(twoPlayerTrade: boolean, tradedPlayers: TradeItem[], dao?: PlayerDAO): Promise<string> {
+        logger.info(`Rendering text for ${tradedPlayers.length} players`);
+        function getMinorLeaguePlayerText(player: Player) {
+            const playerMetaInfo = player.meta ? `(${player.meta?.minorLeaguePlayer?.primary_position} - ${player.meta?.minorLeaguePlayer?.sport} - ${player.meta?.minorLeaguePlayer?.team})` : "(Minors)";
+            const text = `• *${player.name}* ${playerMetaInfo}`;
+            if (!twoPlayerTrade) {
+                const tradedPlayer = tradedPlayers.find(pl => pl.tradeItemId === player.id);
+                return `${text} from _${tradedPlayer?.sender.name}_`;
+            } else {
+                return text;
+            }
+        }
+
+        function getMajorLeaguePlayerText(player: Player) {
+            const playerMetaInfo = player.meta ? `(${player.getEspnEligiblePositions()} - Majors - ${player.mlbTeam})` : "(Majors)";
+            const text =  `• *${player.name}* ${playerMetaInfo}`;
+            if (!twoPlayerTrade) {
+                const tradedPlayer = tradedPlayers.find(pl => pl.tradeItemId === player.id);
+                return `${text} from _${tradedPlayer?.sender.name}_`;
+            } else {
+                return text;
+            }
+        }
+
+        const playerDao = dao || new PlayerDAO();
+        const players = await Promise.all(tradedPlayers.map(async (tradedPlayer: TradeItem) => {
+            return await playerDao.getPlayerById(tradedPlayer.tradeItemId);
+        }));
+logger.debug(inspect(players));
+        const [minorLeaguePlayers, majorLeaguePlayers] = partition(players, player => player.league === PlayerLeagueType.MINOR);
+        const minorLeaguePlayersString = minorLeaguePlayers.map(getMinorLeaguePlayerText).join("\n");
+        const majorLeaguePlayersString = majorLeaguePlayers.map(getMajorLeaguePlayerText).join("\n");
+        return `${majorLeaguePlayersString}\n\n${minorLeaguePlayersString}`.trim();
+    },
+
+    async prepPickText(twoPlayerTrade: boolean, tradedPicks: TradeItem[], dao?: DraftPickDAO): Promise<string> {
+        logger.info(`Rendering text for ${tradedPicks.length} picks`);
+        const pickDao = dao || new DraftPickDAO();
+        if (!twoPlayerTrade) {
+            tradedPicks.sort((tp1, tp2) => tp1.id!.localeCompare(tp2.id!));
+        }
+        const picks = await Promise.all(tradedPicks.map(async (tradedPick: TradeItem) => {
+            return await pickDao.getPickById(tradedPick.tradeItemId);
+        }));
+        if (!twoPlayerTrade) {
+            return zip(tradedPicks, picks)
+                .map(([tradedPick, pick]) => {
+                    return `• *${pick!.originalOwner?.name}'s* ${pick!.season} \
+${ordinal(pick!.round)} round pick from _${tradedPick!.sender.name}_`;
+                })
+                .join("\n")
+                .trim();
+        } else {
+            return picks
+                .map((pick: DraftPick) => {
+                    return `• *${pick.originalOwner?.name}'s* ${pick.season} ${ordinal(pick.round)} round pick`;
+                })
+                .join("\n")
+                .trim();
+        }
+    },
+
+    async getTradeTextForParticipant(twoPlayerTrade: boolean, trade: Trade, participant: TradeParticipant, deps?: TradeFormatterDeps) {
+        logger.info(`Rendering trade items received by ${participant.team.name} for ${trade}`);
+        const header = `*${participant.team.name} receives:*`;
+        const receivedItems = TradeItem.itemsReceivedBy(trade.tradeItems!, participant.team);
+        const playerText = await this.prepPlayerText(twoPlayerTrade, TradeItem.filterPlayers(receivedItems), deps?.playerDao);
+        const pickText = await this.prepPickText(twoPlayerTrade, TradeItem.filterPicks(receivedItems), deps?.pickDao);
+        logger.debug("Header: " + header);
+        logger.debug("Players: " + playerText);
+        logger.debug("Picks: " + pickText);
+        return `${header}\n${playerText ? playerText + "\n\n" : ""}${pickText}`;
+    },
+
+    getSubtitleText(trade: Trade) {
+        logger.info("Rendering subtitle text");
+        function getSlackUsernamesForOwners(owners: User[]) {
+            return owners.map(owner => "<@" + owner.slackUsername + ">").join(", ");
+        }
+        return `*${new Date().toDateString()}* \
+| Trade requested by ${getSlackUsernamesForOwners(trade.creator!.owners!)} \
+- Trading with: ${getSlackUsernamesForOwners(trade.recipients.flatMap(r => r.owners!))}`;
+    },
+
+    getNotificationText: (trade: Trade) => {
+        logger.info("Rendering notification text");
+        return `Trade submitted between ${trade.creator!.name} \
+& ${trade.recipients.map((r: Team) => r.name).join(" & ")}`;
+    },
+
+    getTitleText: () => {
+        logger.info("Rendering title text");
+        return ":loud_sound:  *A Trade Has Been Submitted*  :loud_sound:";
+    },
+};
+
+export default TradeFormatter;
---
+++ tests/unit/slack/tradeFormatter.test.ts
@@ -0,0 +1,105 @@
+import "jest";
+import "jest-extended";
+import logger from "../../../src/bootstrap/logger";
+import TradeFormatter from "../../../src/slack/tradeFormatter";
+import { TradeFactory } from "../../factories/TradeFactory";
+import { UserFactory } from "../../factories/UserFactory";
+import TradeItem from "../../../src/models/tradeItem";
+import DraftPickDAO from "../../../src/DAO/DraftPickDAO";
+import { TeamFactory } from "../../factories/TeamFactory";
+import PlayerDAO from "../../../src/DAO/PlayerDAO";
+
+beforeAll(() => {
+    logger.debug("~~~~~~SLACK TRADE FORMATTER TESTS BEGIN~~~~~~");
+});
+afterAll(() => {
+    logger.debug("~~~~~~SLACK TRADE FORMATTER TESTS COMPLETE~~~~~~");
+});
+
+const trade = TradeFactory.getTrade();
+trade.creator!.owners = [UserFactory.getUser(undefined, undefined, undefined, undefined, {slackUsername: "U12345"})];
+trade.recipients![0].owners = [UserFactory.getUser(undefined, undefined, undefined, undefined, {slackUsername: "U98765"})];
+const tradedPick = trade.picks[0];
+tradedPick.originalOwner = TeamFactory.getTeam();
+const tradedMajorPlayer = trade.majorPlayers[0];
+const tradedMinorPlayer = trade.minorPlayers[0];
+trade.tradeItems?.forEach(it => {
+    it.recipient = trade.tradeItems![0].sender;
+});
+const mockGetPlayerById = jest.fn();
+
+const mockPickDao = {
+    getPickById: jest.fn().mockResolvedValue(tradedPick),
+} as unknown as DraftPickDAO;
+const mockPlayerDao = {
+    getPlayerById: mockGetPlayerById,
+} as unknown as PlayerDAO;
+
+
+describe("Trade Formatter methods", () => {
+    it("getTitleText/0 should return the expected title text", () => {
+        expect(TradeFormatter.getTitleText()).toEqual(":loud_sound:  *A Trade Has Been Submitted*  :loud_sound:");
+    });
+    it("getNotificationText/1 should format the team names involved in the trade", () => {
+        const text = TradeFormatter.getNotificationText(trade);
+        expect(text).toMatch("Trade submitted between");
+        expect(text).toMatch(`${trade.creator!.name} & ${trade.recipients[0]!.name}`);
+    });
+    it("getSubtitleText/1 should format the subtitle with the correct date and slack tags", () => {
+        const text = TradeFormatter.getSubtitleText(trade);
+        expect(text).toMatch("Trade requested by");
+        expect(text).toMatch("Trading with: ");
+        expect(text).toMatch(`<@${trade.creator!.owners![0].slackUsername}>`);
+        expect(text).toMatch(`<@${trade.recipients![0].owners![0].slackUsername}>`);
+        expect(text).toMatch("Trading with: ");
+    });
+    it("prepPickText/3 should format a bullet point list of picks", async () => {
+        const text = await TradeFormatter.prepPickText(true, TradeItem.filterPicks(trade.tradeItems), mockPickDao);
+        expect(text).toMatch("'s");
+        expect(text).toMatch("round pick");
+        expect(text).toMatch(tradedPick.originalOwner?.name!);
+        expect(text).toMatch(tradedPick.season!.toString());
+        expect(text).toMatch(tradedPick.round!.toString());
+        expect(text).not.toMatch("from");
+    });
+    it("prepPickText/3 should include the sender if more than 2-team trade", async () => {
+        const text = await TradeFormatter.prepPickText(false, TradeItem.filterPicks(trade.tradeItems), mockPickDao);
+        expect(text).toMatch("round pick");
+        expect(text).toMatch(tradedPick.originalOwner?.name!);
+        expect(text).toMatch(tradedPick.season!.toString());
+        expect(text).toMatch(tradedPick.round!.toString());
+        expect(text).toMatch("from");
+    });
+    it("prepPlayerText/3 should format a bullet point list of players", async () => {
+        mockGetPlayerById
+            .mockReturnValueOnce(tradedMinorPlayer)
+            .mockReturnValueOnce(tradedMajorPlayer);
+        const text = await TradeFormatter.prepPlayerText(true, TradeItem.filterPlayers(trade.tradeItems), mockPlayerDao);
+        expect(text).toMatch(tradedMajorPlayer.name!);
+        expect(text).toMatch(tradedMinorPlayer.name!);
+        expect(text).toMatch("Majors");
+        expect(text).not.toMatch("from");
+    });
+    it("prepPlayerText/3 should include the sender if more than 2-team trade", async () => {
+        mockGetPlayerById
+            .mockResolvedValueOnce(tradedMajorPlayer)
+            .mockResolvedValueOnce(tradedMinorPlayer);
+        const text = await TradeFormatter.prepPlayerText(false, TradeItem.filterPlayers(trade.tradeItems), mockPlayerDao);
+        expect(text).toMatch(tradedMajorPlayer.name!);
+        expect(text).toMatch(tradedMinorPlayer.name!);
+        expect(text).toMatch("Majors");
+        expect(text).toMatch("from");
+    });
+    it("getTradeTextForParticipant/3 - should render the items received by trade participant and format correctly", async () => {
+        mockGetPlayerById
+            .mockResolvedValueOnce(tradedMajorPlayer)
+            .mockResolvedValueOnce(tradedMinorPlayer);
+        const text = await TradeFormatter.getTradeTextForParticipant(true, trade, trade.tradeParticipants![0], {playerDao: mockPlayerDao, pickDao: mockPickDao});
+        expect(text).toMatch("receives:*");
+        expect(text).toMatch(trade.tradeParticipants![0].team!.name!);
+        expect(text).toMatch("Majors");
+        expect(text).toMatch("Minors");
+        expect(text).toMatch("round pick");
+        expect(text).not.toMatch("from");
+    });
+});
