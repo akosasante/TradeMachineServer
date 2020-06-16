c4c26ea3455d9e8b2f3796b7836aba582657243e
Akosua Asante <akosuaasante@gmail.com>
2020-06-15 23:39:26 -0400

    udpate scheduled jobs logging and bull settings

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,11 @@ import { Server } from "http";
 import startServer from "./bootstrap/app";
 import { setupScheduledEspnUpdates } from "./scheduled_jobs/espnScheduledUpdate";
 import { setupScheduledMlbMinorLeagueUpdates } from "./scheduled_jobs/mlbMinorsScheduledUpdate";
+import { setupEmailConsumers } from "./email/consumers";
 
 const server: Promise<Server> = startServer();
 setupScheduledEspnUpdates();
 setupScheduledMlbMinorLeagueUpdates();
+setupEmailConsumers();
 
 export default server;
