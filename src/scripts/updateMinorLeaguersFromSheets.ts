565343307a7e38a7cbc8b395cf8c296acc271d3a
Akosua <akosasante@users.noreply.github.com>
2021-01-16 20:03:02 -0500

    bugfix

--- src/scripts/updateMinorLeaguersFromSheets.ts
+++ src/scripts/updateMinorLeaguersFromSheets.ts
@@ -3,6 +3,7 @@ import TeamDAO from "../DAO/TeamDAO";
 import PlayerDAO from "../DAO/PlayerDAO";
 import { getCsvFromUrl } from "../db/seed/helpers/csvHelper";
 import { processMinorLeagueCsv } from "../csv/PlayerParser";
+// tslint:disable:no-console
 
 async function run() {
     const args = process.argv.slice(2);
