c664c1324a874e295032f6843d21f5bcc4fe8824
Akosua <akosasante@users.noreply.github.com>
2021-03-27 10:33:37 -0400

    BUGFIX: Allow a larger json limit - Jeremiah made a big trade larger than the default 100kb json body length allowed by the body parser library - Server returned a 500 error in this case (should have been 413, and maybe we could return a better error message to the client in that case but for now, just bumping up the limit to 10mb which should be more than sufficient)

--- src/bootstrap/express.ts
+++ src/bootstrap/express.ts
@@ -18,7 +18,7 @@ app.set("env", process.env.NODE_ENV || "development");
 app.set("json spaces", 2);
 app.set("trust proxy", 1);
 app.use(compression());
-app.use(bodyParser.json());
+app.use(bodyParser.json({limit: "10mb"}));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(morgan("dev", { stream: { write: message => logger.info(message.trim()) } }));
 app.use(responseTime());
