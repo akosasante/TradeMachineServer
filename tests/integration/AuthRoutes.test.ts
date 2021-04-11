2e9c91df5a7771356307b4a31f3f0a3aabe2e351
Akosua <akosasante@users.noreply.github.com>
2021-04-11 17:14:33 -0400

    update authroutes for full coverage. - install jest-date-mock and add it to jest setup files so that we can test expiry stuff easily - export expiry time from user module for test

--- tests/integration/AuthRoutes.test.ts
+++ tests/integration/AuthRoutes.test.ts
@@ -1,21 +1,29 @@
 import { Server } from "http";
-import "jest";
-import "jest-extended";
 import request from "supertest";
 import { redisClient } from "../../src/bootstrap/express";
 import logger from "../../src/bootstrap/logger";
 import UserDAO from "../../src/DAO/UserDAO";
-import User from "../../src/models/user";
+import User, { TIME_TO_EXPIRE_USER_PASSWORD_IN_MS } from "../../src/models/user";
 import startServer from "../../src/bootstrap/app";
-import { makeLoggedInRequest } from "./helpers";
+import { clearDb, makeLoggedInRequest, makePostRequest } from "./helpers";
+import { getConnection } from "typeorm";
+import { generateHashedPassword } from "../../src/authentication/auth";
+import "jest-date-mock";
+import { advanceBy } from "jest-date-mock";
+import {inspect} from "util";
 
 let app: Server;
 let userDAO: UserDAO;
 
 async function shutdown() {
-    await new Promise(resolve => {
-        redisClient.quit(() => {
-            resolve();
+    await new Promise<void>((resolve, reject) => {
+        redisClient.quit((err, reply) => {
+            if (err) {
+                reject(err);
+            } else {
+                logger.debug(`Redis quit successfully with reply ${reply}`);
+                resolve();
+            }
         });
     });
     // redis.quit() creates a thread to close the connection.
@@ -39,49 +47,61 @@ afterAll(async () => {
 });
 
 describe("Auth API endpoints", () => {
+    afterEach(async () => {
+        await clearDb(getConnection(process.env.NODE_ENV));
+    });
+
     const testUser = { email: "test@example.com", password: "lol" };
 
     describe("POST /auth/signup", () => {
+        const signupRequest = (email: string, password: string, status: number = 200) =>
+            (agent: request.SuperTest<request.Test>) =>
+                makePostRequest<Partial<User>>(agent, "/auth/signup", { email, password }, status);
+
         it("should successfully signup the user, set up the session, and return the public user", async () => {
-            const {body} = await request(app)
-                .post("/auth/signup")
-                .send({email: testUser.email, password: testUser.password})
-                .expect(200);
+            const {body} = await signupRequest(testUser.email, testUser.password)(request(app));
 
             expect(body.email).toEqual(testUser.email);
             expect(body).not.toHaveProperty("password");
             expect(body.lastLoggedIn).toBeDefined();
         });
         it("should have created a new user upon signing up if the email wasn't in before", async () => {
+            await signupRequest(testUser.email, testUser.password)(request(app));
+
             const users = await userDAO.getAllUsers();
             expect(users).toHaveLength(1);
         });
         it("should have updated just the password for an existing user with no password", async () => {
-            const email = "test2@example.com";
-            const password = "test";
-            await userDAO.createUsers([{email}]);
-            const noPassUser = await userDAO.findUserWithPassword({email});
+            // Create user directly in db, with just email
+            await userDAO.createUsers([{email: testUser.email}]);
+
+            const noPassUser = await userDAO.findUserWithPassword({email: testUser.email});
             expect(noPassUser!.password).toBeFalsy();
 
-            const {body} = await request(app)
-                .post("/auth/signup")
-                .send({ email, password })
-                .expect(200);
+            // Sign up user
+            const {body} = await signupRequest(testUser.email, testUser.password)(request(app));
             expect(body).not.toHaveProperty("password");
             expect(body.lastLoggedIn).toBeDefined();
 
-            const updatedUser = await userDAO.findUserWithPassword({email});
+            // Check that user password is now set
+            const updatedUser = await userDAO.findUserWithPassword({email: testUser.email});
             expect(updatedUser!.password).toBeDefined();
         });
-        it("should not allow signing up with the same email", async () => {
-            await request(app)
-                .post("/auth/signup")
-                .send({ email: testUser.email, password: "anotha one" })
-                .expect(409);
+        it("should not allow signing up with the same email for an existing user that already has a password", async () => {
+            const hashedPass = await generateHashedPassword(testUser.password);
+            await userDAO.createUsers([{email: testUser.email, password: hashedPass}]);
+
+            await signupRequest(testUser.email, "anotha one", 409)(request(app));
         });
     });
 
     describe("POST /auth/login", () => {
+        const testUser2 = {email: "test2@example.com", password: testUser.password};
+        beforeEach(async() => {
+            const hashedPass = await generateHashedPassword(testUser.password);
+            await userDAO.createUsers([{email: testUser.email, password: hashedPass}, {email: testUser2.email, password: hashedPass}]);
+        });
+
         it("should successfully login the user, set up the session, and return the public user", async () => {
             const {body} = await request(app)
                 .post("/auth/login")
@@ -91,11 +111,30 @@ describe("Auth API endpoints", () => {
             expect(body).not.toHaveProperty("password");
             expect(body.lastLoggedIn).toBeDefined();
         });
-        // TODO: Figure out what to test for when someone logs in to a new user from an existing session
+
+        it("should use the most recently logged in user credentials in the session (if somehow someone logs into a new user from an existing session", async () => {
+            const sessionCheckFn = (agent: request.SuperTest<request.Test>) => agent.get("/auth/session_check").expect(200);
+
+            const { body: user1 } = await makeLoggedInRequest(request.agent(app), testUser.email, testUser.password, sessionCheckFn);
+            expect(user1.email).toEqual(testUser.email);
+            expect(user1).not.toHaveProperty("password");
+            expect(user1.lastLoggedIn).toBeDefined();
+
+            const { body: user2 } = await makeLoggedInRequest(request.agent(app), testUser2.email, testUser2.password, sessionCheckFn);
+            expect(user2.email).toEqual(testUser2.email);
+            expect(user2).not.toHaveProperty("password");
+            expect(user2.lastLoggedIn).toBeDefined();
+        });
     });
 
     describe("POST /auth/logout", () => {
         const logoutFunc = (agent: request.SuperTest<request.Test>) => agent.post("/auth/logout").expect(200);
+
+        beforeEach(async() => {
+            const hashedPass = await generateHashedPassword(testUser.password);
+            await userDAO.createUsers([{email: testUser.email, password: hashedPass}]);
+        });
+
         it("should successfully 'logout' a non-initialized session", async () => {
             await request(app)
                 .post("/auth/logout")
@@ -107,15 +146,16 @@ describe("Auth API endpoints", () => {
     });
 
     describe("POST /auth/reset_password", () => {
-        let user: User|undefined;
-        beforeAll(async () => {
-            const getUser = await userDAO.findUserWithPassword({email: testUser.email});
-            await userDAO.setPasswordExpires(getUser!.id!);
-            user = await userDAO.getUserById(getUser!.id!);
+        beforeEach(async() => {
+            const hashedPass = await generateHashedPassword(testUser.password);
+            await userDAO.createUsers([{email: testUser.email, password: hashedPass}]);
         });
 
         it("should successfully update the user with the hashed password", async () => {
-            const resetPasswordObj = {id: user!.id, password: "newPass", token: user!.passwordResetToken!};
+            const { id } = await userDAO.findUser({email: testUser.email}, true) as User;
+            const { passwordResetToken } = await userDAO.setPasswordExpires(id!);
+            const resetPasswordObj = { id, token: passwordResetToken, password: "newPass"};
+
             const res = await request(app)
                 .post("/auth/reset_password")
                 .send(resetPasswordObj)
@@ -124,15 +164,35 @@ describe("Auth API endpoints", () => {
         });
 
         it("should return a 404 if there's no user with that passwordResetToken", async () => {
-            const resetPasswordObj = {id: user!.id, password: "newPass", token: "xyz-uuid"};
+            const { id } = await userDAO.findUser({email: testUser.email}, true) as User;
+            const resetPasswordObj = {id, token: "xyz-uuid", password: "newPass"};
+
             await request(app)
                 .post("/auth/reset_password")
                 .send(resetPasswordObj)
                 .expect(404);
         });
+
+        it("should return a 403 if there's trying to reset a password past the expiry time", async () => {
+            const { id } = await userDAO.findUser({email: testUser.email}, true) as User;
+            const { passwordResetToken } = await userDAO.setPasswordExpires(id!);
+            const resetPasswordObj = {id, token: passwordResetToken, password: "newPass"};
+
+            advanceBy(TIME_TO_EXPIRE_USER_PASSWORD_IN_MS + 1000);
+
+            await request(app)
+                .post("/auth/reset_password")
+                .send(resetPasswordObj)
+                .expect(403);
+        });
     });
 
-    describe("POST /login/sendResetEmail (send a reset password email)", () => {
+    describe("POST /auth/login/sendResetEmail (send a reset password email)", () => {
+        beforeEach(async() => {
+            const hashedPass = await generateHashedPassword(testUser.password);
+            await userDAO.createUsers([{email: testUser.email, password: hashedPass}]);
+        });
+
         it("should return a 202 message if the email is successfully queued", async () => {
             await request(app)
                 .post("/auth/login/sendResetEmail")
@@ -141,12 +201,36 @@ describe("Auth API endpoints", () => {
         });
     });
 
-    describe("POST /signup/sendEmail (send a registration email)", () => {
+    describe("POST /auth/signup/sendEmail (send a registration email)", () => {
+        beforeEach(async() => {
+            const hashedPass = await generateHashedPassword(testUser.password);
+            await userDAO.createUsers([{email: testUser.email, password: hashedPass}]);
+        });
+
         it("should return a 202 message if the email is successfully queued", async () => {
             await request(app)
                 .post("/auth/signup/sendEmail")
                 .send({email: testUser.email})
                 .expect(202);
+        });
+    });
+
+    describe("GET /auth/session_check",  () => {
+        const sessionCheckFn = (agent: request.SuperTest<request.Test>) => agent.get("/auth/session_check").expect(200);
+
+        beforeEach(async() => {
+            const hashedPass = await generateHashedPassword(testUser.password);
+            await userDAO.createUsers([{email: testUser.email, password: hashedPass}]);
+        });
+
+        it("should return a 200 if a logged in user calls the endpoint", async () => {
+            await makeLoggedInRequest(request.agent(app), testUser.email, testUser.password, sessionCheckFn);
+        });
+
+        it("should return a 403 error if a non-logged in user calls the endpoint", async () => {
+            await request(app)
+                .get("/auth/session_check")
+                .expect(403);
         });
     });
 });
