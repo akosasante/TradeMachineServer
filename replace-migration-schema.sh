d5445a308d38dc3d65249507b100f8dc52d3d07d
Akosua <akosasante@users.noreply.github.com>
2021-04-20 13:09:20 -0400

    update replace migration schema script

--- replace-migration-schema.sh
+++ replace-migration-schema.sh
@@ -2,4 +2,4 @@
 # Example: sh replace-migration-schema.sh staging dev
 # This would replace staging. with dev. in all migration filees
 
-find ./dist/db/migrations/ -iname "*.js" -type f -exec sed -i -e 's/\\"'$1'\\"./\\"'$2'\\"./g' {} \;
+find ./dist/db/migrations/ -iname "*.js" -type f -exec sed -i '' 's/\\"'$1'\\"./\\"'$2'\\"./g' {} \;
