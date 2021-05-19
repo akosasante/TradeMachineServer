1765dbac8e14329ccb59dc2d4794d5cfa763261c
Akosua <akosasante@users.noreply.github.com>
2021-05-19 18:44:24 -0400

    replace migration schema (sed has different schema in linux)

--- replace-migration-schema.sh
+++ replace-migration-schema.sh
@@ -2,4 +2,8 @@
 # Example: sh replace-migration-schema.sh staging dev
 # This would replace staging. with dev. in all migration files
 
-find ./dist/db/migrations/ -iname "*.js" -type f -exec sed -i '' 's/\\"'$1'\\"./\\"'$2'\\"./g' {} \;
+if [[ "$OSTYPE" == "darwin"* ]]; then
+  find ./dist/db/migrations/ -iname "*.js" -type f -exec sed -i '' 's/\\"'$1'\\"./\\"'$2'\\"./g' {} \;
+else
+  find ./dist/db/migrations/ -iname "*.js" -type f -exec sed -i 's/\\"'$1'\\"./\\"'$2'\\"./g' {} \;
+fi;
