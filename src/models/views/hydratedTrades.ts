b8a0a5665919d1fe9aff14ab0d658b6b7e2d5900
Akosua <akosasante@users.noreply.github.com>
2021-06-22 15:05:26 -0400

    fix schema for hdyrated picks

--- src/models/views/hydratedTrades.ts
+++ src/models/views/hydratedTrades.ts
@@ -55,7 +55,7 @@ const expression = `
                     (SELECT "name" FROM "team" tm WHERE tm.id = i."recipientId") AS "recipient",
                     d.*
              FROM "dev"."trade_item" i
-                      RIGHT JOIN "hydrated_picks" d ON d."id" = i."tradeItemId"
+                      RIGHT JOIN "dev"."hydrated_picks" d ON d."id" = i."tradeItemId"
              WHERE i."tradeItemType" = '2'
          ),
          traded_picks AS (
