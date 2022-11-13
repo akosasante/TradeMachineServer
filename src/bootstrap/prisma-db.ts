4235efcd796b2d0a4c822fd758f0026230f9301d
Akosua Asante <akosasante@users.noreply.github.com>
2022-11-13 18:22:44 -0500

    set up prisma and add it to the express global configuration

---
+++ src/bootstrap/prisma-db.ts
@@ -0,0 +1,10 @@
+import { PrismaClient, Prisma } from '@prisma/client';
+
+export default function initializeDb(log = false) {
+    const options: Prisma.PrismaClientOptions = {}
+    if (log) {
+        options.log = ['query', 'info', 'warn', 'error'];
+        options.errorFormat = 'pretty';
+    }
+    return new PrismaClient(options)
+}
