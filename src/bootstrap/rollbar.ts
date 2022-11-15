fc00d59e21522e705080ffc71b078c500de7991f
Akosua Asante <akosasante@users.noreply.github.com>
2022-11-15 14:40:07 -0500

    fix failing tests and reduce memory issues by not having rollbar autoinstrument in test environment

--- src/bootstrap/rollbar.ts
+++ src/bootstrap/rollbar.ts
@@ -3,7 +3,7 @@ import Rollbar from "rollbar";
 export const rollbar = new Rollbar({
     accessToken: process.env.ROLLBAR_TOKEN,
     environment: process.env.ORM_CONFIG,
-    autoInstrument: true, // may want to extend this in the future: https://docs.rollbar.com/docs/nodejs#telemetry
+    autoInstrument: process.env.NODE_ENV === "test" ? false : true, // may want to extend this in the future: https://docs.rollbar.com/docs/nodejs#telemetry; turning off for test since it causes too many eventEmitter listeners (memory hog)
     captureEmail: true,
     captureUsername: true,
     captureUncaught: true,
