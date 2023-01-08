d0a137afcc517113c2b88fc7d76f546d39d3b4a2
Akosua Asante <9094098+akosasante@users.noreply.github.com>
2023-01-08 15:20:50 -0500

    create a prisma-based DAO module to handle getAll (and findAll which is getAll with a filter) queries

--- src/DAO/v2/helpers.ts
+++ src/DAO/v2/helpers.ts
@@ -1,6 +1,13 @@
-export default function exclude<T, Key extends keyof T>(model: T, ...keys: Key[]): Omit<T, Key> {
+export function exclude<T, Key extends keyof T>(model: T, ...keys: Key[]): Omit<T, Key> {
     for (const key of keys) {
         delete model[key];
     }
     return model;
+}
+
+export function convertParamsToWhereQuery(params: string[]): { [field: string]: string }[] {
+    return params.map(paramStr => {
+        const [field, value] = paramStr.split(".");
+        return { [field]: value };
+    });
 }
