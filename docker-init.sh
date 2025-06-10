46ddd32d5db5b9a79009d72cd2445ed62713b72b
Akosua <aaasante@uwaterloo.ca>
2025-06-10 02:02:34 -0400

    formatting

--- docker-init.sh
+++ docker-init.sh
@@ -20,13 +20,12 @@ fi
 
 if [ "$APP_ENV" = "staging" ]; then
   echo "Setting migrations as resolved in staging environment..."
-  echo pwd
-  echo $(ls -la /app/prisma/migrations)
-  export DATABASE_URL="$DATABASE_URL"
-  npx prisma generate prisma/schema.prisma
-  npx prisma migrate resolve --applied 20220620212611_initial_migration
-  npx prisma migrate resolve --applied 20250527131119_remove_uneeded
-  npx prisma migrate resolve --applied 20250527131228_add_views
+  pwd
+  echo $(ls /app/prisma/migrations)
+  DATABASE_URL="$DATABASE_URL" npx prisma generate prisma/schema.prisma
+  DATABASE_URL="$DATABASE_URL" npx prisma migrate resolve --applied 20220620212611_initial_migration
+  DATABASE_URL="$DATABASE_URL" npx prisma migrate resolve --applied 20250527131119_remove_uneeded
+  DATABASE_URL="$DATABASE_URL" npx prisma migrate resolve --applied 20250527131228_add_views
 
 fi
 
