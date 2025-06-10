3f47aa4b89676661ffd6e74f44882ba9b06c7e6b
Akosua <aaasante@uwaterloo.ca>
2025-06-10 12:44:22 -0400

    maybe we don't need to do any further migration steps??

--- docker-init.sh
+++ docker-init.sh
@@ -18,17 +18,6 @@ if [ "$NODE_ENV" = "development" ]; then
     fi
 fi
 
-if [ "$APP_ENV" = "staging" ]; then
-  echo "Setting migrations as resolved in staging environment..."
-#  DATABASE_URL="$DATABASE_URL" npx prisma generate
-  echo $(ls /app/prisma/migrations)
-#  DATABASE_URL=$DATABASE_URL npx prisma generate prisma/schema.prisma
-#  DATABASE_URL=$DATABASE_URL npx prisma migrate resolve --applied 20220620212611_initial_migration
-#  DATABASE_URL=$DATABASE_URL npx prisma migrate resolve --applied 20250527131119_remove_uneeded_tables
-  DATABASE_URL=$DATABASE_URL npx prisma migrate resolve --applied 20250527131228_add_views
-
-fi
-
 # If arguments are provided, start the application
 if [ $# -gt 0 ]; then
     echo "Starting the application..."
