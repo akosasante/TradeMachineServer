bb808a7c3070edd06938efc692c679d298482822
Akosua <aaasante@uwaterloo.ca>
2025-09-29 00:29:26 -0400

    try using a subscriber to add uuid as needed

---
+++ src/db/subscribers/UuidSubscriber.ts
@@ -0,0 +1,23 @@
+import { EntitySubscriberInterface, EventSubscriber, InsertEvent } from "typeorm";
+import { v4 as uuid } from "uuid";
+import { BaseModel } from "../../models/base";
+
+@EventSubscriber()
+export class UuidSubscriber implements EntitySubscriberInterface<BaseModel> {
+    /**
+     * Called before entity insertion.
+     * Ensures all BaseModel entities have UUIDs since TypeORM auto-generation is not working.
+     */
+    beforeInsert(event: InsertEvent<BaseModel>): void {
+        if (event.entity instanceof BaseModel && !event.entity.id) {
+            event.entity.id = uuid();
+        }
+    }
+
+    /**
+     * Indicates that this subscriber only listen to BaseModel events.
+     */
+    listenTo(): typeof BaseModel {
+        return BaseModel;
+    }
+}
