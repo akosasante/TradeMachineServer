9e0ffdff04bab412cf62c26239cc389a245d565a
Akosua <aaasante@uwaterloo.ca>
2025-09-29 01:12:59 -0400

    more fix attempts

--- src/models/base.ts
+++ src/models/base.ts
@@ -1,4 +1,6 @@
-import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import { CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn, BeforeInsert } from "typeorm";
+import { v4 as uuid } from "uuid";
+import logger from "../bootstrap/logger";
 
 export class BaseModel {
     @PrimaryGeneratedColumn("uuid")
@@ -9,6 +11,14 @@ export class BaseModel {
 
     @UpdateDateColumn()
     public dateModified?: Date;
+
+    @BeforeInsert()
+    generateId(): void {
+        if (!this.id) {
+            this.id = uuid();
+            logger.info(`Generated UUID for ${this.constructor.name}: ${this.id}`);
+        }
+    }
 
     public toString(): string {
         return `${this.constructor.name}#${this.id}`;
