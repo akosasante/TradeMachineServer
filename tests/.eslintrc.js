91100c737ffa31a2b57488a2932478ebe5044258
Akosua <aaasante@uwaterloo.ca>
2025-09-30 18:42:31 -0400

    linting

--- tests/.eslintrc.js
+++ tests/.eslintrc.js
@@ -103,7 +103,10 @@ module.exports = {
                 }
             }
         ],
-        "@typescript-eslint/naming-convention": "warn",
+        "@typescript-eslint/naming-convention": ["warn", {
+            selector: "classProperty",
+            format: ["UPPER_CASE"]
+        }],
         "@typescript-eslint/no-array-constructor": "error",
         "@typescript-eslint/no-empty-function": "error",
         "@typescript-eslint/no-empty-interface": "error",
