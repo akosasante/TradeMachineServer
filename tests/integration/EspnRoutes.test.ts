4938a21bc5843f351d822cfef411270422bd8594
Akosua <aaasante@uwaterloo.ca>
2025-10-03 17:03:23 -0400

    use makefile for test-ci to be consistent with unit tests

--- tests/integration/EspnRoutes.test.ts
+++ tests/integration/EspnRoutes.test.ts
@@ -6,20 +6,11 @@ import { adminLoggedIn, clearPrismaDb, makeGetRequest, ownerLoggedIn, setupOwner
 import startServer from "../../src/bootstrap/app";
 import User from "../../src/models/user";
 import initializeDb, { ExtendedPrismaClient } from "../../src/bootstrap/prisma-db";
-import { handleExitInTest } from "../../src/bootstrap/shutdownHandler";
 
 let app: Server;
 let adminUser: User;
 let ownerUser: User;
 let prismaConn: ExtendedPrismaClient;
-async function shutdown() {
-    try {
-        await handleExitInTest();
-    } catch (err) {
-        logger.error(`Error while shutting down: ${err}`);
-    }
-}
-
 beforeAll(async () => {
     logger.debug("~~~~~~ESPN ROUTES BEFORE ALL~~~~~~");
     app = await startServer();
@@ -28,14 +19,16 @@ beforeAll(async () => {
 }, 5000);
 
 afterAll(async () => {
-    logger.debug("~~~~~~ESPN ROUTES AFTER ALL~~~~~~");
-    const shutdownResult = await shutdown();
+    // Only close the server instance for this test file
+    // Shared infrastructure (Redis, Prisma) is cleaned up in globalTeardown
     if (app) {
-        app.close(() => {
-            logger.debug("CLOSED SERVER");
+        return new Promise<void>((resolve) => {
+            app.close(() => {
+                logger.debug("CLOSED SERVER");
+                resolve();
+            });
         });
     }
-    return shutdownResult;
 });
 
 describe("ESPN API endpoints", () => {
